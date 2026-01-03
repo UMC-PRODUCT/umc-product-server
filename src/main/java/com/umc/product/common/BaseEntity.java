b0c146b174d0bac17b667405636aab7e9378d8c8
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2026-01-03 11:19:30 +0900

    feat: BaseEntity에서 id 필드 분리
    추후 id에 대한 변경사항에 유연하게 대처하기 위함임

--- src/main/java/com/umc/product/common/BaseEntity.java
+++ src/main/java/com/umc/product/common/BaseEntity.java
@@ -2,14 +2,9 @@ package com.umc.product.common;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.time.Instant;
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -17,13 +12,9 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
-@Setter(AccessLevel.PROTECTED) // 하위 클래스를 위한 PROTECTED setter 생성
 public abstract class BaseEntity {
 
-    @Id
-    // TODO: 추후 SEQUENCE로 변경해서 성능 개선 추적 by @kyeoungwoon, 2026/01/02
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    // id 필드는 각 entity에서 직접 정의!
 
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
