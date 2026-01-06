db8aaa7bb19157c16e389a8a3d2e80442ab243cb
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2026-01-06 23:45:51 +0900

    feat: ChallengerWorkbookSummary from 메서드 작성

--- src/main/java/com/umc/product/challenger/application/port/in/query/ChallengerWorkbookSummary.java
+++ src/main/java/com/umc/product/challenger/application/port/in/query/ChallengerWorkbookSummary.java
@@ -1,6 +1,10 @@
 package com.umc.product.challenger.application.port.in.query;
 
 
+import com.umc.product.challenger.domain.Challenger;
+import com.umc.product.challenger.domain.ChallengerMission;
+import com.umc.product.challenger.domain.ChallengerWorkbook;
+import com.umc.product.curriculum.domain.OriginalWorkbook;
 import com.umc.product.curriculum.domain.enums.WorkbookStatus;
 
 public record ChallengerWorkbookSummary(
@@ -14,4 +18,26 @@ public record ChallengerWorkbookSummary(
         WorkbookStatus status,
         Boolean isBest
 ) {
+    public static ChallengerWorkbookSummary from(
+            ChallengerWorkbook workbook,
+            ChallengerMission mission,
+            Challenger challenger,
+//           TODO: 나중에 추가
+//            Member member,
+//            School school,
+            OriginalWorkbook originalWorkbook
+    ) {
+        return new ChallengerWorkbookSummary(
+                workbook.getId(),
+                challenger.getId(),
+                null,  // TODO: member.getName()
+                null,  // TODO: school.getName()
+                challenger.getPart().name(),
+                originalWorkbook.getTitle(),
+                mission != null ? mission.getSubmission() : null,
+                workbook.getStatus(),
+                workbook.getIsBest()
+        );
+    }
+
 }
