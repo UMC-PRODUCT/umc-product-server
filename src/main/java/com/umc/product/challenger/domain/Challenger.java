68e99a377173e1e2965cf8ab93a5f1869c8c7f70
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2026-01-06 15:07:19 +0900

    feat: Challenger 도메인 Entity 및 Port 설계

--- src/main/java/com/umc/product/challenger/domain/Challenger.java
+++ src/main/java/com/umc/product/challenger/domain/Challenger.java
@@ -2,13 +2,20 @@ package com.umc.product.challenger.domain;
 
 import com.umc.product.challenger.domain.enums.ChallengerPart;
 import com.umc.product.challenger.domain.enums.ChallengerStatus;
+import com.umc.product.challenger.domain.exception.ChallengerDomainException;
+import com.umc.product.challenger.domain.exception.ChallengerErrorCode;
 import com.umc.product.common.BaseEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -34,5 +41,24 @@ public class Challenger extends BaseEntity {
     @Column(nullable = false, name = "status")
     private ChallengerStatus status;
 
+    @OneToMany(
+            mappedBy = "challenger",
+            fetch = FetchType.LAZY,           // 명시
+            cascade = CascadeType.ALL,        // 명시
+            orphanRemoval = true              // 명시
+    )
+    private List<ChallengerPoint> challengerPoint = new ArrayList<>();
 
+    public Challenger(Long memberId, ChallengerPart part, Long gisuId) {
+        this.memberId = memberId;
+        this.part = part;
+        this.gisuId = gisuId;
+        this.status = ChallengerStatus.ACTIVE;
+    }
+
+    public void validateChallengerStatus() {
+        if (this.status != ChallengerStatus.ACTIVE) {
+            throw new ChallengerDomainException(ChallengerErrorCode.CHALLENGER_NOT_ACTIVE)
+        }
+    }
 }
