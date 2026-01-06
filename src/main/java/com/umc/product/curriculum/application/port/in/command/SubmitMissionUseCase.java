6c3add30d184732847f770b5b1c59dd7f612d40c
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2026-01-07 01:35:37 +0900

    feat: SubmitMissionUseCase JavaDoc 추가

--- src/main/java/com/umc/product/curriculum/application/port/in/command/SubmitMissionUseCase.java
+++ src/main/java/com/umc/product/curriculum/application/port/in/command/SubmitMissionUseCase.java
@@ -4,6 +4,9 @@ public interface SubmitMissionUseCase {
 
     /**
      * 미션 제출
+     *
+     * @param command 미션 제출 커맨드 (미션 ID, 워크북 ID, 제출 내용 포함)
+     * @return 제출된 미션의 ID
      */
     Long submit(SubmitMissionCommand command);
 }
