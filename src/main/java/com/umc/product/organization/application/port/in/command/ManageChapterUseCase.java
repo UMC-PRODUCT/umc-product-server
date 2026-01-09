ae6fc48ec521c898944e04bfe75d2e954498611b
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2026-01-09 17:02:55 +0900

    chore: UseCase 구현체가 없어서 발생하는 오류 해결
    전부 주석처리해서 해결했슨

--- src/main/java/com/umc/product/organization/application/port/in/command/ManageChapterUseCase.java
+++ src/main/java/com/umc/product/organization/application/port/in/command/ManageChapterUseCase.java
@@ -3,6 +3,5 @@ package com.umc.product.organization.application.port.in.command;
 import com.umc.product.organization.application.port.in.command.dto.CreateChapterCommand;
 
 public interface ManageChapterUseCase {
-
-    Long create(CreateChapterCommand command);
+    void create(CreateChapterCommand command);
 }
