d346c45676060ed31499454ca1010f7461ae4ea4
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2026-01-09 15:17:04 +0900

    remove: Challenger 도메인 구현한 부분 충돌 방지를 위해 삭제
    PR #29에 병합 예정

--- src/main/java/com/umc/product/challenger/application/port/out/SaveChallengerWorkbookPort.java
+++
@@ -1,7 +0,0 @@
-package com.umc.product.challenger.application.port.out;
-
-import com.umc.product.challenger.domain.ChallengerWorkbook;
-
-public interface SaveChallengerWorkbookPort {
-    ChallengerWorkbook save(ChallengerWorkbook challengerWorkbook);
-}
