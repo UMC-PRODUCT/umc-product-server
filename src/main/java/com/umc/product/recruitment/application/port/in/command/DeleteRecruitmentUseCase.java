a106162c80b6e3ff25023d4de11cda43a03945d8
galllee <kimim0410@gmail.com>
2026-01-12 05:53:20 +0900

    feat: 모집 삭제 usecase/port

---
+++ src/main/java/com/umc/product/recruitment/application/port/in/command/DeleteRecruitmentUseCase.java
@@ -0,0 +1,5 @@
+package com.umc.product.recruitment.application.port.in.command;
+
+public interface DeleteRecruitmentUseCase {
+    void delete(DeleteRecruitmentCommand command);
+}
--- src/main/java/com/umc/product/recruitment/application/port/out/SaveRecruitmentPort.java
+++ src/main/java/com/umc/product/recruitment/application/port/out/SaveRecruitmentPort.java
@@ -4,4 +4,6 @@ import com.umc.product.recruitment.domain.Recruitment;
 
 public interface SaveRecruitmentPort {
     Recruitment save(Recruitment recruitment);
+
+    void deleteById(Long recruitmentId);
 }
