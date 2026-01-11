4a21ea5c40713220a8efa4664802686c30f25292
sopp1313 <dldpdms0131@gmail.com>
2026-01-11 18:06:36 +0900

    feat: wrapper클래스로 변경

--- src/main/java/com/umc/product/community/application/port/in/BoardsSearchQuery.java
+++ src/main/java/com/umc/product/community/application/port/in/BoardsSearchQuery.java
@@ -5,7 +5,7 @@ import com.umc.product.community.domain.BoardsSortType;
 public record BoardsSearchQuery(
         boolean ing,        // 모집중
         BoardsSortType sort,  // Soft, Hard, ALL
-        int page,
-        int size
+        Integer page,
+        Integer size
 ) {
 }
