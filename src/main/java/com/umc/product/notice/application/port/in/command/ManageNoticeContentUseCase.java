f69b91435c207ac18c8560e26e7bd4b180ff20e4
kanghana1 <qazplm12093@naver.com>
2026-01-13 00:19:02 +0900

    refactor: NoticeContentUseCase에 주석 추가

--- src/main/java/com/umc/product/notice/application/port/in/command/ManageNoticeContentUseCase.java
+++ src/main/java/com/umc/product/notice/application/port/in/command/ManageNoticeContentUseCase.java
@@ -13,15 +13,39 @@ import java.util.List;
 */
 public interface ManageNoticeContentUseCase {
 
+    /*
+    * 공지에 투표 추가
+    * @return 생성된 NoticeVote의 id 리스트
+    */
     List<Long> addVote(AddNoticeVoteCommand command);
 
+    /*
+     * 공지에 이미지 추가
+     * @return 생성된 NoticeImage의 id 리스트
+     */
     List<Long> addImages(AddNoticeImagesCommand command);
 
+    /*
+     * 공지에 링크 추가
+     * @return 생성된 NoticeLink의 id 리스트
+     */
     List<Long> addLinks(AddNoticeLinksCommand command);
 
+    /*
+     * 공지에서 투표 삭제
+     * @return
+     */
     void removeVote(RemoveNoticeVoteCommand command);
 
+    /*
+     * 공지에서 이미지 삭제
+     * @return
+     */
     void removeImage(RemoveNoticeImageCommand command);
 
+    /*
+     * 공지에서 링크 삭제
+     * @return
+     */
     void removeLink(RemoveNoticeLinkCommand command);
 }
