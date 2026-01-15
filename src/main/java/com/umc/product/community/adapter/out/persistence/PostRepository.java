635a75c66b5fb610e4696edc2dde9c4224417bde
Yeeun <dldpdms0131@gmail.com>
2026-01-15 23:53:14 +0900

    [Feat] Community 도메인 Inbound Port 설계 (#51)
    * [feat] 커뮤니티 도메인 기초 설계
    
    * [Feat] Community 파트
    
    * [Feat] Community 파트
    
    * feat: 중복 겹침 문제 해결
    
    중복 제거
    
    * feat: 변화 제거
    
    타입 일체화
    
    * feat: 잘못업로드 된 파일
    
    * [feat] 커뮤니티 port파트 설계
    
    * [feat] 금주 명예의 전당 기초 설계
    
    * feat: wrapper클래스로 변경
    
    * feat: Usecase 구조 변경 및 명칭 일치
    
    * feat: Usecase 구조 변경 및 명칭 일치
    
    * feat: 잘못된 reponse 제거
    
    * feat:잘못된 쿼리 제거
    
    * feat: 중복 파일 제거
    
    * [Feat] 중복 usecase 삭제
    
    * feat: 리뷰 반영 및 명칭 변경
    
    * Fix: Remove all old Boards files and sync with local structure
    
    * Fix: remove Boards files
    
    * Fix: resolve build errors and final code cleanup
    
    * feat: 명칭 변경

---
+++ src/main/java/com/umc/product/community/adapter/out/persistence/PostRepository.java
@@ -0,0 +1,12 @@
+package com.umc.product.community.adapter.out.persistence;
+
+import com.umc.product.community.domain.enums.Category;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<PostJpaEntity, Long> {
+
+    List<PostJpaEntity> findByCategory(Category category);
+
+    List<PostJpaEntity> findByRegion(String region);
+}
