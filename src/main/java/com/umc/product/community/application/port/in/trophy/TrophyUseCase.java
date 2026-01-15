d9b2473228c274aec110bcdacfc250abd8d68a20
sopp1313 <dldpdms0131@gmail.com>
2026-01-15 15:57:11 +0900

    Fix: resolve build errors and final code cleanup

--- src/main/java/com/umc/product/community/application/port/in/trophy/TrophyUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.umc.product.community.application.port.in.trophy;
-
-import java.util.List;
-
-public interface TrophyUseCase {
-    List<TrophyResponse> getTrophies(TrophySearchQuery query);
-
-    TrophyResponse createTrophy(CreateTrophyCommand command);
-}
