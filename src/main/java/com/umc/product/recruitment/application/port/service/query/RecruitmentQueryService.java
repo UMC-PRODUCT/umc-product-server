09025a4c1513e0513e2d19a8fab334dd1be17866
galllee <kimim0410@gmail.com>
2026-01-16 18:14:48 +0900

    refactor: RecruitmentQueryService와 RecruitmentService를 하나로 통합

--- src/main/java/com/umc/product/recruitment/application/port/service/query/RecruitmentQueryService.java
+++ src/main/java/com/umc/product/recruitment/application/port/service/query/RecruitmentQueryService.java
@@ -1,6 +1,9 @@
 package com.umc.product.recruitment.application.port.service.query;
 
+import com.umc.product.recruitment.application.port.in.query.GetActiveRecruitmentUseCase;
 import com.umc.product.recruitment.application.port.in.query.GetRecruitmentNoticeUseCase;
+import com.umc.product.recruitment.application.port.in.query.dto.ActiveRecruitmentInfo;
+import com.umc.product.recruitment.application.port.in.query.dto.GetActiveRecruitmentQuery;
 import com.umc.product.recruitment.application.port.in.query.dto.GetRecruitmentNoticeQuery;
 import com.umc.product.recruitment.application.port.in.query.dto.RecruitmentNoticeInfo;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +13,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class RecruitmentQueryService implements GetRecruitmentNoticeUseCase {
+public class RecruitmentQueryService implements GetActiveRecruitmentUseCase, GetRecruitmentNoticeUseCase {
+
+    @Override
+    public ActiveRecruitmentInfo get(GetActiveRecruitmentQuery query) {
+        return null;
+    }
 
     @Override
     public RecruitmentNoticeInfo get(GetRecruitmentNoticeQuery query) {
