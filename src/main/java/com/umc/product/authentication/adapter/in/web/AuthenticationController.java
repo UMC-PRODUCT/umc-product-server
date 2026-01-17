7e53a0901834fe6ea4a1ce452727c0aa63259a91
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2026-01-18 03:23:22 +0900

    fix: Controller를 사용하던 부분을 RestController로 수정

--- src/main/java/com/umc/product/authentication/adapter/in/web/AuthenticationController.java
+++ src/main/java/com/umc/product/authentication/adapter/in/web/AuthenticationController.java
@@ -16,12 +16,12 @@ import com.umc.product.global.security.annotation.Public;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
 @Tag(name = Constants.AUTH)
