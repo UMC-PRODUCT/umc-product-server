b90f6c5f83d58d4054dd92b06406c28f29838df7
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2026-01-18 04:12:26 +0900

    [Docs] SwaggerTag 세분화 (#103)
    * docs: SwaggerTag 세분화
    
    * typo: 오타 수정

--- src/main/java/com/umc/product/global/constant/SwaggerTag.java
+++ src/main/java/com/umc/product/global/constant/SwaggerTag.java
@@ -8,15 +8,21 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum SwaggerTag {
 
-    TEST("개발자용", "개발자용 Test API", 0),
-    AUTH("인증/인가", "인증/인가 API", 2),
-    MEMBER("사용자", "사용자 API", 1),
-    CHALLENGER("챌린저", "챌린저 API", 2),
-    ORGANIZATION("조직 (학교/지부/중앙운영사무국)", "조직 관련 API", 2),
-    CURRICULUM("커리큘럼", "커리큘럼 관련 API", 2),
-    SCHEDULE("행사 및 스터디 일정", "행사 및 스터디 관련 API", 2),
-    COMMUNITY("커뮤니티", "커뮤니티 API", 2),
-    NOTICE("공지사항", "공지사항 API", 2),
+    TEST("개발자용", "temp | 개발자용 Test API", 0),
+    AUTH("인증/인가", "authentication | 인증/인가 API", 1),
+    MEMBER("회원", "member | 회원 API", 2),
+    CHALLENGER("챌린저", "challenger | 챌린저 API", 3),
+    ORGANIZATION("조직 (학교/지부/중앙운영사무국)", "organization | 조직 관련 API", 4),
+    CURRICULUM("커리큘럼", "curriculum | 커리큘럼 관련 API", 5),
+    SCHEDULE("행사 및 스터디 일정", "schedule | 행사 및 스터디 관련 API", 6),
+    COMMUNITY("커뮤니티", "community | 커뮤니티 API", 7),
+    NOTICE("공지사항", "notice | 공지사항 API", 8),
+    SURVEY("설문", "survey | 설문 API", 9),
+    RECRUITMENT("모집", "recruitment | 모집 API", 10),
+    NOTIFICATION("알람", "notification | 알림 API", 11),
+    STORAGE("파일 저장소", "storage | 파일 저장 API", 12),
+    PROJECT("UPMS", "project | 프로젝트 매칭 API", 13),
+
 
     ADMIN("관리자/운영진", "관리자 전용 별도 API", 99),
 
@@ -51,18 +57,23 @@ public enum SwaggerTag {
     /**
      * Annotation에서 사용할 수 있도록 상수로 정의
      *
-     * @Tag(name = SwaggerTag.Constants.USER)
+     * @Tag(name = SwaggerTag.Constants.MEMBER)
      */
     public static class Constants {
-        public static final String TEST = "개발자용";
-        public static final String AUTH = "인증/인가";
-        public static final String MEMBER = "사용자";
-        public static final String CHALLENGER = "챌린저";
-        public static final String ORGANIZATION = "조직 (학교/지부/중앙운영사무국)";
-        public static final String CURRICULUM = "커리큘럼";
-        public static final String SCHEDULE = "행사 및 스터디 일정";
-        public static final String COMMUNITY = "커뮤니티";
-        public static final String NOTICE = "공지사항";
-        public static final String ADMIN = "관리자/운영진";
+        public static final String TEST = SwaggerTag.TEST.getName();
+        public static final String AUTH = SwaggerTag.AUTH.getName();
+        public static final String MEMBER = SwaggerTag.MEMBER.getName();
+        public static final String CHALLENGER = SwaggerTag.CHALLENGER.getName();
+        public static final String ORGANIZATION = SwaggerTag.ORGANIZATION.getName();
+        public static final String CURRICULUM = SwaggerTag.CURRICULUM.getName();
+        public static final String SCHEDULE = SwaggerTag.SCHEDULE.getName();
+        public static final String COMMUNITY = SwaggerTag.COMMUNITY.getName();
+        public static final String NOTICE = SwaggerTag.NOTICE.getName();
+        public static final String SURVEY = SwaggerTag.SURVEY.getName();
+        public static final String RECRUITMENT = SwaggerTag.RECRUITMENT.getName();
+        public static final String NOTIFICATION = SwaggerTag.NOTIFICATION.getName();
+        public static final String STORAGE = SwaggerTag.STORAGE.getName();
+        public static final String PROJECT = SwaggerTag.PROJECT.getName();
+        public static final String ADMIN = SwaggerTag.ADMIN.getName();
     }
 }
