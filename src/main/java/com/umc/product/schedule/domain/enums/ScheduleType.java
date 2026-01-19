0dffa5ed8197328b0ab16991ad1cebee3eadbff4
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2026-01-19 21:05:13 +0900

    feat: 화면 기준으로 ScheduleType 추가

--- src/main/java/com/umc/product/schedule/domain/enums/ScheduleType.java
+++ src/main/java/com/umc/product/schedule/domain/enums/ScheduleType.java
@@ -4,5 +4,11 @@ public enum ScheduleType {
     STUDY_SESSION,
     CENTRAL_EVENT,
     CHAPTER_EVENT,
-    SCHOOL_EVENT
+    SCHOOL_EVENT,
+
+    // 화면 기준 추가
+    TEAM_ACTIVITY,      // 팀 활동
+    PERSONAL_STUDY,     // 개인 학습
+    MENTORING,          // 멘토링
+    EVENT               // 행사
 }
