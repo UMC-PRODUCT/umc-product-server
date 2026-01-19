d4ee31b8cd1384cda668648838cb177c9f79e99c
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2026-01-19 23:10:13 +0900

    feat: AttendanceRecord, AttendanceSheet JpaRepository에 메서드 추가

--- src/main/java/com/umc/product/schedule/adapter/out/persistence/AttendanceRecordJpaRepository.java
+++ src/main/java/com/umc/product/schedule/adapter/out/persistence/AttendanceRecordJpaRepository.java
@@ -1,7 +1,10 @@
 package com.umc.product.schedule.adapter.out.persistence;
 
 import com.umc.product.schedule.domain.AttendanceRecord;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AttendanceRecordJpaRepository extends JpaRepository<AttendanceRecord, Long> {
+
+    List<AttendanceRecord> findByAttendanceSheetId(Long sheetId);
 }
--- src/main/java/com/umc/product/schedule/adapter/out/persistence/AttendanceSheetJpaRepository.java
+++ src/main/java/com/umc/product/schedule/adapter/out/persistence/AttendanceSheetJpaRepository.java
@@ -1,7 +1,9 @@
 package com.umc.product.schedule.adapter.out.persistence;
 
 import com.umc.product.schedule.domain.AttendanceSheet;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AttendanceSheetJpaRepository extends JpaRepository<AttendanceSheet, Long> {
+    Optional<AttendanceSheet> findByScheduleId(Long scheduleId);
 }
