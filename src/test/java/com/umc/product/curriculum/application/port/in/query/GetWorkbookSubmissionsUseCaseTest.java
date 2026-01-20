811f642e84e7249b7a45ff5417879a3f58d60ade
박지현 <102128060+wlgusqkr@users.noreply.github.com>
2026-01-20 17:52:52 +0900

    test: 테스트 수정

--- src/test/java/com/umc/product/curriculum/application/port/in/query/GetWorkbookSubmissionsUseCaseTest.java
+++ src/test/java/com/umc/product/curriculum/application/port/in/query/GetWorkbookSubmissionsUseCaseTest.java
@@ -2,9 +2,9 @@ package com.umc.product.curriculum.application.port.in.query;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.umc.product.challenger.adapter.out.persistence.ChallengerWorkbookJpaRepository;
 import com.umc.product.challenger.domain.Challenger;
 import com.umc.product.common.domain.enums.ChallengerPart;
+import com.umc.product.curriculum.adapter.out.persistence.ChallengerWorkbookJpaRepository;
 import com.umc.product.curriculum.adapter.out.persistence.CurriculumJpaRepository;
 import com.umc.product.curriculum.adapter.out.persistence.OriginalWorkbookJpaRepository;
 import com.umc.product.curriculum.application.port.in.query.dto.GetWorkbookSubmissionsQuery;
