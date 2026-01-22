a66bde60efdfdafbceec2b451f06f8f7367be901
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2026-01-23 05:59:49 +0900

    feat: 월별 일정 목록 조회 usecase 작성

---
+++ src/main/java/com/umc/product/schedule/application/port/in/query/GetMyScheduleCalendarUseCase.java
@@ -0,0 +1,9 @@
+package com.umc.product.schedule.application.port.in.query;
+
+import com.umc.product.schedule.application.port.in.query.dto.MyScheduleCalendarInfo;
+import java.util.List;
+
+public interface GetMyScheduleCalendarUseCase {
+
+    List<MyScheduleCalendarInfo> getMyMonthlySchedules(Long memberId, int year, int month);
+}
