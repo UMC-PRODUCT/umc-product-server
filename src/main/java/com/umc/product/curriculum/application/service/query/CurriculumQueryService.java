9461117f11a8e0fc7599b890e375dd76b168f5cc
박지현 <102128060+wlgusqkr@users.noreply.github.com>
2026-01-27 01:54:57 +0900

    feat: 내 커리큘럼 조회 API 추가

--- src/main/java/com/umc/product/curriculum/application/service/query/CurriculumQueryService.java
+++ src/main/java/com/umc/product/curriculum/application/service/query/CurriculumQueryService.java
@@ -1,5 +1,7 @@
 package com.umc.product.curriculum.application.service.query;
 
+import com.umc.product.challenger.application.port.in.query.GetChallengerUseCase;
+import com.umc.product.challenger.application.port.in.query.dto.ChallengerInfo;
 import com.umc.product.curriculum.application.port.in.query.CurriculumProgressInfo;
 import com.umc.product.curriculum.application.port.in.query.GetCurriculumProgressUseCase;
 import com.umc.product.curriculum.application.port.out.LoadCurriculumProgressPort;
@@ -12,14 +14,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class CurriculumQueryService implements GetCurriculumProgressUseCase {
 
-//    private final GetChallengerUseCase getChallengerUseCase;
+    private final GetChallengerUseCase getChallengerUseCase;
     private final LoadCurriculumProgressPort loadCurriculumProgressPort;
 
     @Override
     public CurriculumProgressInfo getMyProgress(Long challengerId) {
-//        ChallengerInfo challengerInfo = getChallengerUseCase.getChallengerPublicInfo(challengerId);
+        ChallengerInfo challengerInfo = getChallengerUseCase.getChallengerPublicInfo(challengerId);
 
-        return null;
-//        return loadCurriculumProgressPort.findCurriculumProgress(challengerId, challengerInfo.part());
+        return loadCurriculumProgressPort.findCurriculumProgress(challengerId, challengerInfo.part());
     }
 }
