49d0cb5e934e4f58740bd2ac4afd3771eddb82ca
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2026-01-27 23:41:45 +0900

    feat: getAgreedTermsByMemberId 메소드에서 하나의 타입에 2개 이상의 active=true 약관이 있을 시 최근 거를 선택하도록 병합 로직 추가

--- src/main/java/com/umc/product/terms/application/service/query/TermsAgreementQueryService.java
+++ src/main/java/com/umc/product/terms/application/service/query/TermsAgreementQueryService.java
@@ -39,9 +39,9 @@ public class TermsAgreementQueryService implements GetTermsAgreementUseCase {
         // 해당 타입들의 활성 약관 상세 정보 가져오기
         List<Terms> activeTermsList = loadTermsPort.findAllActiveByTypes(types);
 
-        // Map<타입, 약관객체> 형태로 변환
+        // Map<타입, 약관객체> 형태로 변환 (중복 발생 시 최근 거 선택)
         Map<TermsType, Terms> termsMap = activeTermsList.stream()
-                .collect(Collectors.toMap(Terms::getType, terms -> terms));
+                .collect(Collectors.toMap(Terms::getType, terms -> terms, (oldValue, newValue) -> newValue));
 
         // 동의 내역을 순회하며 Map에서 데이터를 꺼내 TermsInfo 만들기
         return consents.stream()
