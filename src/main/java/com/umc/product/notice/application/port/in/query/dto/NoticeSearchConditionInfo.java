1dbf318bc0183ee8f1f13b1847c02bc72dd0ea39
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2026-02-02 02:13:24 +0900

    remove: Notice 도메인에서 사용되지 않는 DTO 제거

--- src/main/java/com/umc/product/notice/application/port/in/query/dto/NoticeSearchConditionInfo.java
+++
@@ -1,8 +0,0 @@
-package com.umc.product.notice.application.port.in.query.dto;
-
-import com.umc.product.notice.domain.enums.NoticeClassification;
-
-public record NoticeSearchConditionInfo(
-    NoticeClassification category
-) {
-}
