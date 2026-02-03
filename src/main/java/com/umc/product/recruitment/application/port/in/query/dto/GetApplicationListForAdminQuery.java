f1cc0ef4c7d45afb7db67f262f2096328a306923
galllee <kimim0410@gmail.com>
2026-02-04 00:38:37 +0900

    feat: 총괄 지원자 관리 리스트 조회 API 컨트롤러/usecaes 구현

---
+++ src/main/java/com/umc/product/recruitment/application/port/in/query/dto/GetApplicationListForAdminQuery.java
@@ -0,0 +1,15 @@
+package com.umc.product.recruitment.application.port.in.query.dto;
+
+import com.umc.product.recruitment.application.port.in.PartOption;
+
+public record GetApplicationListForAdminQuery(
+        Long recruitmentId,
+        Long chapterId,
+        Long schoolId,
+        PartOption part,
+        String keyword,
+        int page,
+        int limit,
+        Long requesterId
+) {
+}
