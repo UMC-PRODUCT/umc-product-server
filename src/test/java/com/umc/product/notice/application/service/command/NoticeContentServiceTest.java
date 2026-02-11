84c7203aee60a999cf08359d7a203a554f6320df
kanghana1 <qazplm12093@naver.com>
2026-02-11 10:57:21 +0900

    feat: 공지 단위테스트 구현

---
+++ src/test/java/com/umc/product/notice/application/service/command/NoticeContentServiceTest.java
@@ -0,0 +1,417 @@
+package com.umc.product.notice.application.service.command;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.never;
+
+import com.umc.product.notice.application.port.in.command.dto.AddNoticeImagesCommand;
+import com.umc.product.notice.application.port.in.command.dto.AddNoticeLinksCommand;
+import com.umc.product.notice.application.port.in.command.dto.AddNoticeVotesCommand;
+import com.umc.product.notice.application.port.in.command.dto.ReplaceNoticeImagesCommand;
+import com.umc.product.notice.application.port.in.command.dto.ReplaceNoticeLinksCommand;
+import com.umc.product.notice.application.port.in.command.dto.ReplaceNoticeVotesCommand;
+import com.umc.product.notice.application.port.out.LoadNoticeImagePort;
+import com.umc.product.notice.application.port.out.LoadNoticeLinkPort;
+import com.umc.product.notice.application.port.out.LoadNoticePort;
+import com.umc.product.notice.application.port.out.LoadNoticeVotePort;
+import com.umc.product.notice.application.port.out.SaveNoticeImagePort;
+import com.umc.product.notice.application.port.out.SaveNoticeLinkPort;
+import com.umc.product.notice.application.port.out.SaveNoticeVotePort;
+import com.umc.product.notice.domain.Notice;
+import com.umc.product.notice.domain.NoticeImage;
+import com.umc.product.notice.domain.NoticeLink;
+import com.umc.product.notice.domain.NoticeVote;
+import com.umc.product.notice.domain.exception.NoticeDomainException;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+@ExtendWith(MockitoExtension.class)
+class NoticeContentServiceTest {
+
+    @Mock LoadNoticeVotePort loadNoticeVotePort;
+    @Mock LoadNoticeLinkPort loadNoticeLinkPort;
+    @Mock LoadNoticeImagePort loadNoticeImagePort;
+    @Mock SaveNoticeVotePort saveNoticeVotePort;
+    @Mock SaveNoticeImagePort saveNoticeImagePort;
+    @Mock SaveNoticeLinkPort saveNoticeLinkPort;
+    @Mock LoadNoticePort loadNoticePort;
+
+    @InjectMocks NoticeContentService sut;
+
+    private static final Long NOTICE_ID = 100L;
+
+    private Notice createNotice() {
+        Notice notice = Notice.create("테스트 공지", "테스트 내용", 10L, false);
+        ReflectionTestUtils.setField(notice, "id", NOTICE_ID);
+        return notice;
+    }
+
+    @Nested
+    @DisplayName("addImages")
+    class AddImagesTest {
+
+        @Test
+        void 공지사항에_이미지를_추가한다() {
+            // given
+            Notice notice = createNotice();
+            var command = new AddNoticeImagesCommand(List.of("img1", "img2"));
+
+            given(loadNoticePort.findNoticeById(NOTICE_ID)).willReturn(Optional.of(notice));
+            given(loadNoticeImagePort.countImageByNoticeId(NOTICE_ID)).willReturn(0);
+            given(loadNoticeImagePort.findNextImageDisplayOrder(NOTICE_ID)).willReturn(0);
+            given(saveNoticeImagePort.saveAllImages(any())).willAnswer(inv -> {
+                List<NoticeImage> images = inv.getArgument(0);
+                for (int i = 0; i < images.size(); i++) {
+                    ReflectionTestUtils.setField(images.get(i), "id", (long) (i + 1));
+                }
+                return images;
+            });
+
+            // when
+            List<Long> result = sut.addImages(command, NOTICE_ID);
+
+            // then
+            assertThat(result).hasSize(2);
+            assertThat(result).containsExactly(1L, 2L);
+        }
+
+        @Test
+        void 이미지가_10개를_초과하면_예외가_발생한다() {
+            // given
+            Notice notice = createNotice();
+            var command = new AddNoticeImagesCommand(List.of("img1", "img2", "img3"));
+
+            given(loadNoticePort.findNoticeById(NOTICE_ID)).willReturn(Optional.of(notice));
+            given(loadNoticeImagePort.countImageByNoticeId(NOTICE_ID)).willReturn(8);
+
+            // when & then
+            assertThatThrownBy(() -> sut.addImages(command, NOTICE_ID))
+                    .isInstanceOf(NoticeDomainException.class);
+        }
+
+        @Test
+        void 이미지_ID_목록이_비어있으면_예외가_발생한다() {
+            // given
+            Notice notice = createNotice();
+            var command = new AddNoticeImagesCommand(List.of());
+
+            given(loadNoticePort.findNoticeById(NOTICE_ID)).willReturn(Optional.of(notice));
+
+            // when & then
+            assertThatThrownBy(() -> sut.addImages(command, NOTICE_ID))
+                    .isInstanceOf(NoticeDomainException.class);
+        }
+
+        @Test
+        void 이미지_ID_목록이_null이면_예외가_발생한다() {
+            // given
+            Notice notice = createNotice();
+            var command = new AddNoticeImagesCommand(null);
+
+            given(loadNoticePort.findNoticeById(NOTICE_ID)).willReturn(Optional.of(notice));
+
+            // when & then
+            assertThatThrownBy(() -> sut.addImages(command, NOTICE_ID))
+                    .isInstanceOf(NoticeDomainException.class);
+        }
+
+        @Test
+        void 기존_이미지와_합쳐서_정확히_10개면_성공한다() {
+            // given
+            Notice notice = createNotice();
+            var command = new AddNoticeImagesCommand(List.of("img1", "img2"));
+
+            given(loadNoticePort.findNoticeById(NOTICE_ID)).willReturn(Optional.of(notice));
+            given(loadNoticeImagePort.countImageByNoticeId(NOTICE_ID)).willReturn(8);
+            given(loadNoticeImagePort.findNextImageDisplayOrder(NOTICE_ID)).willReturn(8);
+            given(saveNoticeImagePort.saveAllImages(any())).willAnswer(inv -> {
+                List<NoticeImage> images = inv.getArgument(0);
+                for (int i = 0; i < images.size(); i++) {
+                    ReflectionTestUtils.setField(images.get(i), "id", (long) (i + 1));
+                }
+                return images;
+            });
+
+            // when
+            List<Long> result = sut.addImages(command, NOTICE_ID);
+
+            // then
+            assertThat(result).hasSize(2);
+        }
+    }
+
+    @Nested
+    @DisplayName("addLinks")
+    class AddLinksTest {
+
+        @Test
+        void 공지사항에_링크를_추가한다() {
+            // given
+            Notice notice = createNotice();
+            var command = new AddNoticeLinksCommand(List.of("https://example.com", "https://test.com"));
+
+            given(loadNoticePort.findNoticeById(NOTICE_ID)).willReturn(Optional.of(notice));
+            given(loadNoticeLinkPort.findNextLinkDisplayOrder(NOTICE_ID)).willReturn(0);
+            given(saveNoticeLinkPort.saveAllLinks(any())).willAnswer(inv -> {
+                List<NoticeLink> links = inv.getArgument(0);
+                for (int i = 0; i < links.size(); i++) {
+                    ReflectionTestUtils.setField(links.get(i), "id", (long) (i + 1));
+                }
+                return links;
+            });
+
+            // when
+            List<Long> result = sut.addLinks(command, NOTICE_ID);
+
+            // then
+            assertThat(result).hasSize(2);
+        }
+
+        @Test
+        void 링크_목록이_비어있으면_예외가_발생한다() {
+            // given
+            Notice notice = createNotice();
+            var command = new AddNoticeLinksCommand(List.of());
+
+            given(loadNoticePort.findNoticeById(NOTICE_ID)).willReturn(Optional.of(notice));
+
+            // when & then
+            assertThatThrownBy(() -> sut.addLinks(command, NOTICE_ID))
+                    .isInstanceOf(NoticeDomainException.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("addVotes")
+    class AddVotesTest {
+
+        @Test
+        void 공지사항에_투표를_추가한다() {
+            // given
+            Notice notice = createNotice();
+            var command = new AddNoticeVotesCommand(List.of(1L, 2L));
+
+            given(loadNoticePort.findNoticeById(NOTICE_ID)).willReturn(Optional.of(notice));
+            given(loadNoticeVotePort.findNextVoteDisplayOrder(NOTICE_ID)).willReturn(0);
+            given(saveNoticeVotePort.saveAllVotes(any())).willAnswer(inv -> {
+                List<NoticeVote> votes = inv.getArgument(0);
+                for (int i = 0; i < votes.size(); i++) {
+                    ReflectionTestUtils.setField(votes.get(i), "id", (long) (i + 1));
+                }
+                return votes;
+            });
+
+            // when
+            List<Long> result = sut.addVotes(command, NOTICE_ID);
+
+            // then
+            assertThat(result).hasSize(2);
+        }
+
+        @Test
+        void 투표_ID_목록이_비어있으면_예외가_발생한다() {
+            // given
+            Notice notice = createNotice();
+            var command = new AddNoticeVotesCommand(List.of());
+
+            given(loadNoticePort.findNoticeById(NOTICE_ID)).willReturn(Optional.of(notice));
+
+            // when & then
+            assertThatThrownBy(() -> sut.addVotes(command, NOTICE_ID))
+                    .isInstanceOf(NoticeDomainException.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("removeContentsByNoticeId")
+    class RemoveContentsTest {
+
+        @Test
+        void 공지사항의_모든_콘텐츠를_삭제한다() {
+            // when
+            sut.removeContentsByNoticeId(NOTICE_ID);
+
+            // then
+            then(saveNoticeImagePort).should().deleteAllImagesByNoticeId(NOTICE_ID);
+            then(saveNoticeLinkPort).should().deleteAllLinksByNoticeId(NOTICE_ID);
+            then(saveNoticeVotePort).should().deleteAllVotesByNoticeId(NOTICE_ID);
+        }
+    }
+
+    @Nested
+    @DisplayName("replaceImages")
+    class ReplaceImagesTest {
+
+        @Test
+        void 공지사항_이미지를_전체_교체한다() {
+            // given
+            Notice notice = createNotice();
+            var command = new ReplaceNoticeImagesCommand(List.of("newImg1", "newImg2"));
+
+            given(loadNoticePort.findNoticeById(NOTICE_ID)).willReturn(Optional.of(notice));
+
+            // when
+            sut.replaceImages(command, NOTICE_ID);
+
+            // then
+            then(saveNoticeImagePort).should().deleteAllImagesByNoticeId(NOTICE_ID);
+            then(saveNoticeImagePort).should().saveAllImages(any());
+        }
+
+        @Test
+        void 교체할_이미지_목록이_null이면_아무_작업도_하지_않는다() {
+            // given
+            var command = new ReplaceNoticeImagesCommand(null);
+
+            // when
+            sut.replaceImages(command, NOTICE_ID);
+
+            // then
+            then(saveNoticeImagePort).should(never()).deleteAllImagesByNoticeId(anyLong());
+        }
+
+        @Test
+        void 교체할_이미지_목록이_빈_리스트면_기존_이미지만_삭제한다() {
+            // given
+            Notice notice = createNotice();
+            var command = new ReplaceNoticeImagesCommand(List.of());
+
+            given(loadNoticePort.findNoticeById(NOTICE_ID)).willReturn(Optional.of(notice));
+
+            // when
+            sut.replaceImages(command, NOTICE_ID);
+
+            // then
+            then(saveNoticeImagePort).should().deleteAllImagesByNoticeId(NOTICE_ID);
+            then(saveNoticeImagePort).should(never()).saveAllImages(any());
+        }
+
+        @Test
+        void 교체할_이미지가_10개를_초과하면_예외가_발생한다() {
+            // given
+            List<String> tooManyImages = List.of(
+                    "i1", "i2", "i3", "i4", "i5", "i6", "i7", "i8", "i9", "i10", "i11");
+            var command = new ReplaceNoticeImagesCommand(tooManyImages);
+
+            // when & then
+            assertThatThrownBy(() -> sut.replaceImages(command, NOTICE_ID))
+                    .isInstanceOf(NoticeDomainException.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("replaceLinks")
+    class ReplaceLinksTest {
+
+        @Test
+        void 공지사항_링크를_전체_교체한다() {
+            // given
+            Notice notice = createNotice();
+            var command = new ReplaceNoticeLinksCommand(List.of("https://new1.com", "https://new2.com"));
+
+            given(loadNoticePort.findNoticeById(NOTICE_ID)).willReturn(Optional.of(notice));
+
+            // when
+            sut.replaceLinks(command, NOTICE_ID);
+
+            // then
+            then(saveNoticeLinkPort).should().deleteAllLinksByNoticeId(NOTICE_ID);
+            then(saveNoticeLinkPort).should().saveAllLinks(any());
+        }
+
+        @Test
+        void 교체할_링크_목록이_null이면_아무_작업도_하지_않는다() {
+            // given
+            var command = new ReplaceNoticeLinksCommand(null);
+
+            // when
+            sut.replaceLinks(command, NOTICE_ID);
+
+            // then
+            then(saveNoticeLinkPort).should(never()).deleteAllLinksByNoticeId(anyLong());
+        }
+    }
+
+    @Nested
+    @DisplayName("replaceVotes")
+    class ReplaceVotesTest {
+
+        @Test
+        void 공지사항_투표를_전체_교체한다() {
+            // given
+            Notice notice = createNotice();
+            var command = new ReplaceNoticeVotesCommand(List.of(10L, 20L));
+
+            given(loadNoticePort.findNoticeById(NOTICE_ID)).willReturn(Optional.of(notice));
+
+            // when
+            sut.replaceVotes(command, NOTICE_ID);
+
+            // then
+            then(saveNoticeVotePort).should().deleteAllVotesByNoticeId(NOTICE_ID);
+            then(saveNoticeVotePort).should().saveAllVotes(any());
+        }
+
+        @Test
+        void 교체할_투표_목록이_null이면_아무_작업도_하지_않는다() {
+            // given
+            var command = new ReplaceNoticeVotesCommand(null);
+
+            // when
+            sut.replaceVotes(command, NOTICE_ID);
+
+            // then
+            then(saveNoticeVotePort).should(never()).deleteAllVotesByNoticeId(anyLong());
+        }
+    }
+
+    @Nested
+    @DisplayName("존재하지 않는 공지사항")
+    class NotFoundTest {
+
+        @Test
+        void 존재하지_않는_공지사항에_이미지_추가_시_예외가_발생한다() {
+            // given
+            var command = new AddNoticeImagesCommand(List.of("img1"));
+            given(loadNoticePort.findNoticeById(NOTICE_ID)).willReturn(Optional.empty());
+
+            // when & then
+            assertThatThrownBy(() -> sut.addImages(command, NOTICE_ID))
+                    .isInstanceOf(NoticeDomainException.class);
+        }
+
+        @Test
+        void 존재하지_않는_공지사항에_링크_추가_시_예외가_발생한다() {
+            // given
+            var command = new AddNoticeLinksCommand(List.of("https://test.com"));
+            given(loadNoticePort.findNoticeById(NOTICE_ID)).willReturn(Optional.empty());
+
+            // when & then
+            assertThatThrownBy(() -> sut.addLinks(command, NOTICE_ID))
+                    .isInstanceOf(NoticeDomainException.class);
+        }
+
+        @Test
+        void 존재하지_않는_공지사항에_투표_추가_시_예외가_발생한다() {
+            // given
+            var command = new AddNoticeVotesCommand(List.of(1L));
+            given(loadNoticePort.findNoticeById(NOTICE_ID)).willReturn(Optional.empty());
+
+            // when & then
+            assertThatThrownBy(() -> sut.addVotes(command, NOTICE_ID))
+                    .isInstanceOf(NoticeDomainException.class);
+        }
+    }
+}
---
+++ src/test/java/com/umc/product/notice/application/service/command/NoticeReadServiceTest.java
@@ -0,0 +1,118 @@
+package com.umc.product.notice.application.service.command;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.never;
+
+import com.umc.product.challenger.application.port.in.query.GetChallengerUseCase;
+import com.umc.product.challenger.application.port.in.query.dto.ChallengerInfo;
+import com.umc.product.common.domain.enums.ChallengerPart;
+import com.umc.product.notice.application.port.in.query.GetNoticeTargetUseCase;
+import com.umc.product.notice.application.port.out.LoadNoticePort;
+import com.umc.product.notice.application.port.out.LoadNoticeReadPort;
+import com.umc.product.notice.application.port.out.SaveNoticeReadPort;
+import com.umc.product.notice.domain.Notice;
+import com.umc.product.notice.domain.NoticeRead;
+import com.umc.product.notice.domain.exception.NoticeDomainException;
+import com.umc.product.notice.dto.NoticeTargetInfo;
+import java.util.List;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+@ExtendWith(MockitoExtension.class)
+class NoticeReadServiceTest {
+
+    @Mock LoadNoticePort loadNoticePort;
+    @Mock LoadNoticeReadPort loadNoticeReadPort;
+    @Mock SaveNoticeReadPort saveNoticeReadPort;
+    @Mock GetChallengerUseCase getChallengerUseCase;
+    @Mock GetNoticeTargetUseCase getNoticeTargetUseCase;
+
+    @InjectMocks NoticeReadService sut;
+
+    private static final Long NOTICE_ID = 100L;
+    private static final Long MEMBER_ID = 1L;
+    private static final Long CHALLENGER_ID = 10L;
+    private static final Long GISU_ID = 7L;
+
+    private Notice createNotice() {
+        Notice notice = Notice.create("테스트 공지", "테스트 내용", 10L, false);
+        ReflectionTestUtils.setField(notice, "id", NOTICE_ID);
+        return notice;
+    }
+
+    private ChallengerInfo createChallengerInfo() {
+        return ChallengerInfo.builder()
+                .challengerId(CHALLENGER_ID)
+                .memberId(MEMBER_ID)
+                .gisuId(GISU_ID)
+                .part(ChallengerPart.SPRINGBOOT)
+                .build();
+    }
+
+    private NoticeTargetInfo createTargetInfo() {
+        return new NoticeTargetInfo(GISU_ID, null, null, List.of());
+    }
+
+    @Nested
+    @DisplayName("recordRead")
+    class RecordReadTest {
+
+        @Test
+        void 공지사항_읽음을_기록한다() {
+            // given
+            Notice notice = createNotice();
+            NoticeTargetInfo targetInfo = createTargetInfo();
+
+            given(loadNoticePort.findNoticeById(NOTICE_ID)).willReturn(Optional.of(notice));
+            given(getNoticeTargetUseCase.findByNoticeId(NOTICE_ID)).willReturn(targetInfo);
+            given(getChallengerUseCase.getActiveByMemberIdAndGisuId(MEMBER_ID, GISU_ID))
+                    .willReturn(createChallengerInfo());
+            given(loadNoticeReadPort.existsRead(NOTICE_ID, CHALLENGER_ID)).willReturn(false);
+
+            // when
+            sut.recordRead(NOTICE_ID, MEMBER_ID);
+
+            // then
+            then(saveNoticeReadPort).should().saveRead(any(NoticeRead.class));
+        }
+
+        @Test
+        void 이미_읽은_공지사항은_중복_저장하지_않는다() {
+            // given
+            Notice notice = createNotice();
+            NoticeTargetInfo targetInfo = createTargetInfo();
+
+            given(loadNoticePort.findNoticeById(NOTICE_ID)).willReturn(Optional.of(notice));
+            given(getNoticeTargetUseCase.findByNoticeId(NOTICE_ID)).willReturn(targetInfo);
+            given(getChallengerUseCase.getActiveByMemberIdAndGisuId(MEMBER_ID, GISU_ID))
+                    .willReturn(createChallengerInfo());
+            given(loadNoticeReadPort.existsRead(NOTICE_ID, CHALLENGER_ID)).willReturn(true);
+
+            // when
+            sut.recordRead(NOTICE_ID, MEMBER_ID);
+
+            // then
+            then(saveNoticeReadPort).should(never()).saveRead(any());
+        }
+
+        @Test
+        void 존재하지_않는_공지사항_읽음_처리_시_예외가_발생한다() {
+            // given
+            given(loadNoticePort.findNoticeById(NOTICE_ID)).willReturn(Optional.empty());
+
+            // when & then
+            Assertions.assertThatThrownBy(() -> sut.recordRead(NOTICE_ID, MEMBER_ID))
+                    .isInstanceOf(NoticeDomainException.class);
+        }
+    }
+}
