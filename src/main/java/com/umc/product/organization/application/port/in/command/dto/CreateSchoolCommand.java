661bee7d08ff6ff99f4b95a8bb481f1e11ab7348
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2026-02-12 03:32:37 +0900

    feat: bulk method들 추가

--- src/main/java/com/umc/product/organization/application/port/in/command/dto/CreateSchoolCommand.java
+++ src/main/java/com/umc/product/organization/application/port/in/command/dto/CreateSchoolCommand.java
@@ -1,11 +1,13 @@
 package com.umc.product.organization.application.port.in.command.dto;
 
 import java.util.List;
+import lombok.Builder;
 
+@Builder
 public record CreateSchoolCommand(
-        String schoolName,
-        String remark,
-        String logoImageId,
-        List<SchoolLinkCommand> links
+    String schoolName,
+    String remark,
+    String logoImageId,
+    List<SchoolLinkCommand> links
 ) {
 }
