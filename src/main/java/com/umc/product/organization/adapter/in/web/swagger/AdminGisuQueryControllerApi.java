2ae7d14d9068ee1fcf7d7b47aa1419d1a5a52c5d
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2026-02-15 03:42:16 +0900

    [Feat] 기수 ID로 기수 정보를 조회하는 API 추가 (#425)
    feat: 기수 ID로 기수를 조회하는 API 추가

--- src/main/java/com/umc/product/organization/adapter/in/web/swagger/AdminGisuQueryControllerApi.java
+++ src/main/java/com/umc/product/organization/adapter/in/web/swagger/AdminGisuQueryControllerApi.java
@@ -1,8 +1,10 @@
 package com.umc.product.organization.adapter.in.web.swagger;
 
+import com.umc.product.global.security.annotation.Public;
 import com.umc.product.organization.adapter.in.web.dto.response.ActiveGisuResponse;
 import com.umc.product.organization.adapter.in.web.dto.response.GisuNameListResponse;
 import com.umc.product.organization.adapter.in.web.dto.response.GisuPageResponse;
+import com.umc.product.organization.adapter.in.web.dto.response.GisuResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -10,9 +12,15 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Tag(name = "Organization | 기수 Query", description = "")
 public interface AdminGisuQueryControllerApi {
+
+    @Public
+    @GetMapping("/{gisuId}")
+    GisuResponse getGisu(@PathVariable Long gisuId);
 
     @Operation(summary = "기수 목록 조회 ", description = "기수 목록을 최신순(generation 내림차순)으로 페이징 조회합니다")
     @ApiResponses(value = {
