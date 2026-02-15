6ccbc6a17d1d741a1aa57ee806d734f287921275
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2026-02-16 01:22:38 +0900

    chore: 불필요한 PathVariable 명시 제거

--- src/main/java/com/umc/product/test/controller/TestController.java
+++ src/main/java/com/umc/product/test/controller/TestController.java
@@ -73,21 +73,21 @@ public class TestController {
     @Operation(summary = "AccessToken 발급")
     @Public
     @GetMapping("/token/access/{memberId}")
-    public String getAccessToken(@PathVariable("memberId") Long memberId) {
+    public String getAccessToken(@PathVariable Long memberId) {
         return jwtTokenProvider.createAccessToken(memberId, null);
     }
 
     @Operation(summary = "RefreshToken 발급")
     @Public
     @GetMapping("/token/refresh/{memberId}")
-    public String getRefreshToken(@PathVariable("memberId") Long memberId) {
+    public String getRefreshToken(@PathVariable Long memberId) {
         return jwtTokenProvider.createRefreshToken(memberId);
     }
 
     @Operation(summary = "EmailVerificationToken 발급")
     @Public
     @GetMapping("/token/email/{email}")
-    public String getEmailVerification(@PathVariable("email") String email) {
+    public String getEmailVerification(@PathVariable String email) {
         return jwtTokenProvider.createEmailVerificationToken(email);
     }
 
