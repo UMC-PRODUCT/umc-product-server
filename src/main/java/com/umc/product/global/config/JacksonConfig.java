e25bb4dc3df740018e935c0444f693ddda3497a4
sopp1313 <dldpdms0131@gmail.com>
2026-02-19 13:32:18 +0900

    refactor: 응답 형식통일

--- src/main/java/com/umc/product/global/config/JacksonConfig.java
+++ src/main/java/com/umc/product/global/config/JacksonConfig.java
@@ -1,6 +1,7 @@
 package com.umc.product.global.config;
 
 import com.fasterxml.jackson.core.json.JsonWriteFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +11,8 @@ public class JacksonConfig {
 
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
-        return builder -> builder.featuresToEnable(
-                JsonWriteFeature.WRITE_NUMBERS_AS_STRINGS.mappedFeature()
-        );
+        return builder -> builder
+                .featuresToEnable(JsonWriteFeature.WRITE_NUMBERS_AS_STRINGS.mappedFeature())
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);  // ISO 8601 형식으로 직렬화
     }
 }
