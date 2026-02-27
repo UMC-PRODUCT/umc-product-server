403ddb0cc8e79ec3fbccbcb68e4a5ce979ab88bc
Kyeoung Woon Park <saveearth1@cau.ac.kr>
2026-02-28 07:01:11 +0900

    feat: 지부를 벌크로 생성할 수 있는 API 추가

--- src/main/java/com/umc/product/organization/adapter/in/web/swagger/AdminChapterControllerApi.java
+++ src/main/java/com/umc/product/organization/adapter/in/web/swagger/AdminChapterControllerApi.java
@@ -6,6 +6,9 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "Organization | 지부 Command", description = "")
 public interface AdminChapterControllerApi {
@@ -17,7 +20,10 @@ public interface AdminChapterControllerApi {
         @ApiResponse(responseCode = "404", description = "기수 또는 학교를 찾을 수 없음"),
         @ApiResponse(responseCode = "409", description = "지부명 중복 또는 학교가 이미 다른 지부에 배정됨")
     })
-    Long createChapter(CreateChapterRequest request);
+    Long createChapter(@Valid CreateChapterRequest request);
+
+    @Operation(summary = "지부 일괄 생성")
+    List<Long> createChapterBulk(@RequestBody List<CreateChapterRequest> requests);
 
     @Operation(summary = "지부 삭제", description = "지부를 삭제합니다. 소속 학교는 삭제되지 않습니다.")
     @ApiResponses(value = {
