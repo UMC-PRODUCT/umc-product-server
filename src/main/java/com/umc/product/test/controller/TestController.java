68109f8fed75d861a098ba8f0cd117333e301aa4
Kyeoung Woon Park <saveearth1@cau.ac.kr>
2026-03-01 01:19:50 +0900

    chore: 테스트용 컨트롤러 API 정리

--- src/main/java/com/umc/product/test/controller/TestController.java
+++ src/main/java/com/umc/product/test/controller/TestController.java
@@ -1,11 +1,7 @@
 package com.umc.product.test.controller;
 
 import com.umc.product.authentication.adapter.out.external.AppleTokenVerifier;
-import com.umc.product.authorization.adapter.in.aspect.CheckAccess;
-import com.umc.product.authorization.domain.PermissionType;
-import com.umc.product.authorization.domain.ResourceType;
 import com.umc.product.challenger.application.port.in.query.GetChallengerUseCase;
-import com.umc.product.challenger.application.port.in.query.dto.ChallengerInfo;
 import com.umc.product.common.domain.enums.OAuthProvider;
 import com.umc.product.global.response.ApiResponse;
 import com.umc.product.global.security.JwtTokenProvider;
@@ -18,7 +14,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,45 +32,18 @@ public class TestController {
     private final AppleTokenVerifier appleTokenVerifier;
 
     @GetMapping("apple-client-secret")
+    @Operation(summary = "Apple Client Secret 생성")
     String getAppleClientSecret() {
         return appleTokenVerifier.generateClientSecret();
-    }
-
-    @GetMapping("permission/notice-read")
-    @CheckAccess(
-        resourceType = ResourceType.NOTICE,
-        resourceId = "#noticeId", // SpEL 표현식 - 공부하세요!!
-        permission = PermissionType.READ,
-        message = "하나야 스트레스 많이 받을거야~ 자기 전에도 생각 날꺼야~ 도움 많이 될꺼야~"
-    )
-    void permissionTest(Long noticeId) {
-    }
-
-    @GetMapping("permission/no-evaluator-test")
-    @CheckAccess(
-        resourceType = ResourceType.CURRICULUM,
-        resourceId = "#noticeId",
-        permission = PermissionType.DELETE,
-        message = "하나야 스트레스 많이 받을거야~ 자기 전에도 생각 날꺼야~ 도움 많이 될꺼야~"
-    )
-    void noEvaluatorForPermission(Long something) {
-    }
-
-    @GetMapping("challenger")
-    @Operation(summary = "memberId와 gisuId로 챌린저 정보 조회")
-    public ChallengerInfo getChallengerByMemberAndGisuId(
-        Long memberId, Long gisuId
-    ) {
-        return getChallengerUseCase.getByMemberIdAndGisuId(
-            memberId, gisuId
-        );
     }
 
     @Public
     @Operation(summary = "AccessToken 발급")
     @GetMapping("/token/access")
-    public String getAccessToken(@RequestParam Long memberId,
-                                 @RequestParam(required = false) Long expirationInMinutes) {
+    public String getAccessToken(
+        @RequestParam Long memberId,
+        @RequestParam(required = false) Long expirationInMinutes
+    ) {
         return expirationInMinutes == null ?
             // null이면 기본값
             jwtTokenProvider.createAccessToken(memberId, null) :
@@ -85,15 +53,15 @@ public class TestController {
 
     @Operation(summary = "RefreshToken 발급")
     @Public
-    @GetMapping("/token/refresh/{memberId}")
-    public String getRefreshToken(@PathVariable Long memberId) {
+    @GetMapping("/token/refresh")
+    public String getRefreshToken(@RequestParam Long memberId) {
         return jwtTokenProvider.createRefreshToken(memberId);
     }
 
     @Operation(summary = "EmailVerificationToken 발급")
     @Public
-    @GetMapping("/token/email/{email}")
-    public String getEmailVerification(@PathVariable String email) {
+    @GetMapping("/token/email")
+    public String getEmailVerification(@RequestParam String email) {
         return jwtTokenProvider.createEmailVerificationToken(email);
     }
 
