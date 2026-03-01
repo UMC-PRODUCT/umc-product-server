5a9f78c24abdaff284df9b87c340bc36cbf7c7ff
박지현 <102128060+wlgusqkr@users.noreply.github.com>
2026-03-01 15:57:15 +0900

    feat: 학교관리에 권한 추가

---
+++ src/main/java/com/umc/product/organization/application/service/evaluator/SchoolPermissionEvaluator.java
@@ -0,0 +1,40 @@
+package com.umc.product.organization.application.service.evaluator;
+
+import com.umc.product.authorization.application.port.in.query.GetChallengerRoleUseCase;
+import com.umc.product.authorization.application.port.out.ResourcePermissionEvaluator;
+import com.umc.product.authorization.domain.ResourcePermission;
+import com.umc.product.authorization.domain.ResourceType;
+import com.umc.product.authorization.domain.SubjectAttributes;
+import com.umc.product.authorization.domain.exception.AuthorizationDomainException;
+import com.umc.product.authorization.domain.exception.AuthorizationErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class SchoolPermissionEvaluator implements ResourcePermissionEvaluator {
+
+    private final GetChallengerRoleUseCase getChallengerRoleUseCase;
+
+    @Override
+    public ResourceType supportedResourceType() {
+        return ResourceType.SCHOOL;
+    }
+
+    @Override
+    public boolean evaluate(SubjectAttributes subjectAttributes, ResourcePermission resourcePermission) {
+        if (!resourcePermission.resourceType().getSupportedPermissions()
+            .contains(resourcePermission.permission())) {
+            throw new AuthorizationDomainException(AuthorizationErrorCode.INVALID_RESOURCE_PERMISSION_GIVEN,
+                "SchoolPermissionEvaluator에서 지원하지 않는 권한 유형에 대한 평가가 시도되었습니다: " + resourcePermission.permission());
+        }
+
+        return switch (resourcePermission.permission()) {
+            case WRITE, EDIT, DELETE -> getChallengerRoleUseCase.isCentralCore(subjectAttributes.memberId());
+            default -> throw new AuthorizationDomainException(AuthorizationErrorCode.PERMISSION_TYPE_NOT_IMPLEMENTED,
+                "SchoolPermissionEvaluator에서 해당 PermissionType을 지원하지 않습니다: " + resourcePermission.permission());
+        };
+    }
+}
