21200b248acb1934ecd28f783d89db7f062682ca
박지현 <102128060+wlgusqkr@users.noreply.github.com>
2026-03-08 21:04:04 +0900

    refactor: 없는 기능인 기수 정보 수정 기능 테스트 제거

--- src/test/java/com/umc/product/organization/adapter/in/web/AdminGisuControllerTest.java
+++ src/test/java/com/umc/product/organization/adapter/in/web/AdminGisuControllerTest.java
@@ -3,7 +3,6 @@ package com.umc.product.organization.adapter.in.web;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
@@ -12,10 +11,8 @@ import static org.springframework.restdocs.request.RequestDocumentation.pathPara
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.umc.product.organization.adapter.in.web.dto.request.CreateGisuRequest;
-import com.umc.product.organization.adapter.in.web.dto.request.UpdateGisuRequest;
 import com.umc.product.support.DocumentationTest;
 import java.time.Instant;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
@@ -45,26 +42,6 @@ class AdminGisuControllerTest extends DocumentationTest {
                         fieldWithPath("number").type(JsonFieldType.STRING).description("기수 번호"),
                         fieldWithPath("startAt").type(JsonFieldType.STRING).description("기수 시작일시"),
                         fieldWithPath("endAt").type(JsonFieldType.STRING).description("기수 종료일시"))));
-    }
-
-    @Test
-    @Disabled("아직 없는 기능")
-    void 기수_정보를_수정한다() throws Exception {
-        // given
-        Long gisuId = 1L;
-        UpdateGisuRequest request = new UpdateGisuRequest(START_AT, Instant.parse("2025-09-30T23:59:59Z"));
-
-        // when
-        ResultActions result = mockMvc.perform(
-                patch("/api/v1/gisu/{gisuId}", gisuId)
-                        .content(objectMapper.writeValueAsString(request))
-                        .contentType(MediaType.APPLICATION_JSON));
-
-        // then
-        result.andExpect(status().isOk())
-                .andDo(restDocsHandler.document(pathParameters(parameterWithName("gisuId").description("기수 ID")),
-                        requestFields(fieldWithPath("startAt").type(JsonFieldType.STRING).description("기수 시작일시"),
-                                fieldWithPath("endAt").type(JsonFieldType.STRING).description("기수 종료일시"))));
     }
 
     @Test
