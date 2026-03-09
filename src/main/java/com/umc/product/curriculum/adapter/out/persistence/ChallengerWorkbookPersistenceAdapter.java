2c83fe7e68b864ef75e27a2d28bff5cf44004533
Kyeoung Woon Park <saveearth1@cau.ac.kr>
2026-03-10 00:04:25 +0900

    fix: curriculum 도메인 내 BusinessException을 사용하지 않는 문제 해결

--- src/main/java/com/umc/product/curriculum/adapter/out/persistence/ChallengerWorkbookPersistenceAdapter.java
+++ src/main/java/com/umc/product/curriculum/adapter/out/persistence/ChallengerWorkbookPersistenceAdapter.java
@@ -3,9 +3,8 @@ package com.umc.product.curriculum.adapter.out.persistence;
 import com.umc.product.curriculum.application.port.out.LoadChallengerWorkbookPort;
 import com.umc.product.curriculum.application.port.out.SaveChallengerWorkbookPort;
 import com.umc.product.curriculum.domain.ChallengerWorkbook;
+import com.umc.product.curriculum.domain.exception.CurriculumDomainException;
 import com.umc.product.curriculum.domain.exception.CurriculumErrorCode;
-import com.umc.product.global.exception.BusinessException;
-import com.umc.product.global.exception.constant.Domain;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -18,7 +17,8 @@ public class ChallengerWorkbookPersistenceAdapter implements LoadChallengerWorkb
 
     @Override
     public ChallengerWorkbook findById(Long id) {
-        return challengerWorkbookJpaRepository.findById(id).orElseThrow(() -> new BusinessException(Domain.CURRICULUM, CurriculumErrorCode.CHALLENGER_WORKBOOK_NOT_FOUND));
+        return challengerWorkbookJpaRepository.findById(id)
+            .orElseThrow(() -> new CurriculumDomainException(CurriculumErrorCode.CHALLENGER_WORKBOOK_NOT_FOUND));
     }
 
     @Override
