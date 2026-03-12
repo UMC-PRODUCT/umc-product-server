6e29a04a891e7f43257006965d15917414b1afb4
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2026-03-12 09:43:19 +0900

    feat: controller api description, usecase 주석 수정

--- src/main/java/com/umc/product/schedule/adapter/in/web/swagger/ScheduleQueryControllerApi.java
+++ src/main/java/com/umc/product/schedule/adapter/in/web/swagger/ScheduleQueryControllerApi.java
@@ -15,8 +15,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Tag(name = "Schedule | 일정 Query", description = "")
 public interface ScheduleQueryControllerApi {
 
-    @Operation(summary = "일정 목록 조회",
-        description = "출석 통계와 함께 일정 목록을 조회합니다. 중앙 운영진은 기수 전체 일정, 학교 운영진은 본인 생성 일정만 조회됩니다.")
+    @Operation(summary = "출석 통계와 함께 일정 목록 조회",
+        description = """
+            출석 통계와 함께 일정 목록을 조회합니다.
+            - 중앙 운영진 : 본인 참석 일정
+            - 교내 회장단 : 교내 챌린저가 파트장으로 있는 스터디 그룹 일정 + 본인이 생성한 일정
+            - 교내 파트장 : 본인이 파트장으로 있는 스터디 그룹 일정 + 본인이 생성한 일정
+            - 기타 운영진 : 본인이 생성한 일정
+            """)
     List<ScheduleListResponse> getScheduleList(@CurrentMember MemberPrincipal memberPrincipal);
 
     @Operation(summary = "월별 내 일정 캘린더/리스트 조회",
