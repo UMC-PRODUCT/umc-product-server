31e353ddc063b74243bc208c3dc556bbfeca3861
kanghana1 <qazplm12093@naver.com>
2026-03-13 15:49:11 +0900

    fix: 피드백 일부 반영

--- src/main/java/com/umc/product/notice/application/service/query/NoticeQueryService.java
+++ src/main/java/com/umc/product/notice/application/service/query/NoticeQueryService.java
@@ -103,7 +103,7 @@ public class NoticeQueryService implements GetNoticeUseCase {
             imageInfos,
             linkInfos,
             targetInfo,
-            notice.getViewCount() + 1,
+            notice.getViewCount(),
             notice.getCreatedAt()
         );
     }
