d73e8f64d07c9615272da5fb23cf8f849d861054
galllee <kimim0410@gmail.com>
2026-03-16 02:16:35 +0900

    chore: import문 수정

--- src/main/java/com/umc/product/survey/adapter/in/web/VoteController.java
+++ src/main/java/com/umc/product/survey/adapter/in/web/VoteController.java
@@ -19,7 +19,14 @@ import com.umc.product.survey.application.port.in.query.dto.VoteInfo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/v1/surveys/votes")
