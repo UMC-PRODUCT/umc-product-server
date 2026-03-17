884b77d28f00d7b3d5f75c4d2126efdee451ee1e
박지현 <102128060+wlgusqkr@users.noreply.github.com>
2026-03-17 15:17:04 +0900

    refactor: 안쓰는 API 제거

--- src/test/java/com/umc/product/organization/application/port/in/query/GetStudyGroupUseCaseTest.java
+++ src/test/java/com/umc/product/organization/application/port/in/query/GetStudyGroupUseCaseTest.java
@@ -4,8 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.umc.product.challenger.domain.Challenger;
 import com.umc.product.common.domain.enums.ChallengerPart;
-import com.umc.product.organization.application.port.in.query.dto.PartSummaryInfo;
-import com.umc.product.organization.application.port.in.query.dto.SchoolStudyGroupInfo;
 import com.umc.product.organization.application.port.in.query.dto.StudyGroupListInfo;
 import com.umc.product.organization.application.port.in.query.dto.StudyGroupListQuery;
 import com.umc.product.organization.application.port.in.query.dto.StudyGroupNameInfo;
@@ -44,130 +42,6 @@ class GetStudyGroupUseCaseTest extends UseCaseTestSupport {
 
     @Autowired
     private LoadStudyGroupPort loadStudyGroupPort;
-
-    @Test
-    void 활성_기수의_스터디_그룹이_있는_학교_목록을_조회한다() {
-        // given
-        Gisu activeGisu = gisuFixture.활성_기수(9L);
-        School school1 = manageSchoolPort.save(School.create("서울대학교", "비고1"));
-        School school2 = manageSchoolPort.save(School.create("연세대학교", "비고2"));
-        manageSchoolPort.save(School.create("고려대학교", "비고3")); // 스터디 그룹 없음
-
-        Challenger seoulChallenger1 = challengerFixture.챌린저(
-            memberFixture.학교_소속_멤버("서울대생1", school1.getId()).getId(), ChallengerPart.WEB, activeGisu.getId());
-        Challenger seoulChallenger2 = challengerFixture.챌린저(
-            memberFixture.학교_소속_멤버("서울대생2", school1.getId()).getId(), ChallengerPart.WEB, activeGisu.getId());
-        Challenger yonseiChallenger1 = challengerFixture.챌린저(
-            memberFixture.학교_소속_멤버("연세대생1", school2.getId()).getId(), ChallengerPart.SPRINGBOOT, activeGisu.getId());
-
-        studyGroupFixture.스터디그룹("React A팀", activeGisu, ChallengerPart.WEB, seoulChallenger1.getId());
-        studyGroupFixture.스터디그룹("React B팀", activeGisu, ChallengerPart.WEB, seoulChallenger2.getId());
-        studyGroupFixture.스터디그룹("Spring A팀", activeGisu, ChallengerPart.SPRINGBOOT, yonseiChallenger1.getId());
-
-        // when
-        List<SchoolStudyGroupInfo> result = getStudyGroupUseCase.getSchools();
-
-        // then
-        assertThat(result).hasSize(2);
-
-        SchoolStudyGroupInfo seoul = result.stream()
-            .filter(s -> s.schoolName().equals("서울대학교"))
-            .findFirst()
-            .orElseThrow();
-        assertThat(seoul.totalStudyGroupCount()).isEqualTo(2);
-
-        SchoolStudyGroupInfo yonsei = result.stream()
-            .filter(s -> s.schoolName().equals("연세대학교"))
-            .findFirst()
-            .orElseThrow();
-        assertThat(yonsei.totalStudyGroupCount()).isEqualTo(1);
-    }
-
-    @Test
-    void 비활성_기수의_스터디_그룹은_학교_목록에_포함되지_않는다() {
-        // given
-        Gisu inactiveGisu = gisuFixture.비활성_기수(8L);
-        Gisu activeGisu = gisuFixture.활성_기수(9L);
-        School school1 = manageSchoolPort.save(School.create("서울대학교", "비고1"));
-        School school2 = manageSchoolPort.save(School.create("연세대학교", "비고2"));
-
-        Challenger oldChallenger = challengerFixture.챌린저(
-            memberFixture.학교_소속_멤버("서울대생1", school1.getId()).getId(), ChallengerPart.WEB, inactiveGisu.getId());
-        Challenger newChallenger = challengerFixture.챌린저(
-            memberFixture.학교_소속_멤버("연세대생1", school2.getId()).getId(), ChallengerPart.WEB, activeGisu.getId());
-
-        studyGroupFixture.스터디그룹("Old팀", inactiveGisu, ChallengerPart.WEB, oldChallenger.getId());
-        studyGroupFixture.스터디그룹("New팀", activeGisu, ChallengerPart.WEB, newChallenger.getId());
-
-        // when
-        List<SchoolStudyGroupInfo> result = getStudyGroupUseCase.getSchools();
-
-        // then
-        assertThat(result).hasSize(1);
-        assertThat(result.get(0).schoolName()).isEqualTo("연세대학교");
-    }
-
-    @Test
-    void 특정_학교의_파트별_스터디_그룹_요약을_조회한다() {
-        // given
-        Gisu activeGisu = gisuFixture.활성_기수(9L);
-        School school = manageSchoolPort.save(School.create("서울대학교", "비고1"));
-
-        Challenger webChallenger1 = challengerFixture.챌린저(
-            memberFixture.학교_소속_멤버("웹1", school.getId()).getId(), ChallengerPart.WEB, activeGisu.getId());
-        Challenger webChallenger2 = challengerFixture.챌린저(
-            memberFixture.학교_소속_멤버("웹2", school.getId()).getId(), ChallengerPart.WEB, activeGisu.getId());
-        Challenger springChallenger = challengerFixture.챌린저(
-            memberFixture.학교_소속_멤버("스프링1", school.getId()).getId(), ChallengerPart.SPRINGBOOT, activeGisu.getId());
-        Challenger iosChallenger = challengerFixture.챌린저(
-            memberFixture.학교_소속_멤버("iOS1", school.getId()).getId(), ChallengerPart.IOS, activeGisu.getId());
-
-        studyGroupFixture.스터디그룹("React A팀", activeGisu, ChallengerPart.WEB, webChallenger1.getId());
-        studyGroupFixture.스터디그룹("React B팀", activeGisu, ChallengerPart.WEB, webChallenger2.getId());
-        studyGroupFixture.스터디그룹("Spring A팀", activeGisu, ChallengerPart.SPRINGBOOT, springChallenger.getId());
-        studyGroupFixture.스터디그룹("iOS A팀", activeGisu, ChallengerPart.IOS, iosChallenger.getId());
-
-        // when
-        PartSummaryInfo result = getStudyGroupUseCase.getParts(school.getId());
-
-        // then
-        assertThat(result.schoolId()).isEqualTo(school.getId());
-        assertThat(result.schoolName()).isEqualTo("서울대학교");
-        assertThat(result.parts()).hasSize(3);
-
-        PartSummaryInfo.PartInfo webPart = result.parts().stream()
-            .filter(p -> p.part() == ChallengerPart.WEB)
-            .findFirst()
-            .orElseThrow();
-        assertThat(webPart.studyGroupCount()).isEqualTo(2);
-
-        PartSummaryInfo.PartInfo springPart = result.parts().stream()
-            .filter(p -> p.part() == ChallengerPart.SPRINGBOOT)
-            .findFirst()
-            .orElseThrow();
-        assertThat(springPart.studyGroupCount()).isEqualTo(1);
-    }
-
-    @Test
-    void 활성_기수_스터디_그룹이_없는_학교는_빈_파트_목록을_반환한다() {
-        // given
-        Gisu inactiveGisu = gisuFixture.비활성_기수(8L);
-        gisuFixture.활성_기수(9L);
-        School school = manageSchoolPort.save(School.create("서울대학교", "비고1"));
-
-        Challenger oldChallenger = challengerFixture.챌린저(
-            memberFixture.학교_소속_멤버("서울대생1", school.getId()).getId(), ChallengerPart.WEB, inactiveGisu.getId());
-
-        studyGroupFixture.스터디그룹("Old팀", inactiveGisu, ChallengerPart.WEB, oldChallenger.getId());
-
-        // when
-        PartSummaryInfo result = getStudyGroupUseCase.getParts(school.getId());
-
-        // then
-        assertThat(result.schoolId()).isEqualTo(school.getId());
-        assertThat(result.schoolName()).isEqualTo("서울대학교");
-        assertThat(result.parts()).isEmpty();
-    }
 
     @Test
     void 스터디_그룹_목록을_커서_기반으로_조회한다() {
