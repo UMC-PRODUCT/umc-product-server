48d05a46cdad372bf97cd482085bc935bb61fada
Kyeoung Woon Park <saveearth1@cau.ac.kr>
2026-04-12 23:52:55 +0900

    feat: 지원서 상태값을 나타내는 enum 값 변경

--- src/main/java/com/umc/product/project/domain/enums/ProjectApplicationStatus.java
+++ src/main/java/com/umc/product/project/domain/enums/ProjectApplicationStatus.java
@@ -1,9 +1,10 @@
 package com.umc.product.project.domain.enums;
 
 public enum ProjectApplicationStatus {
-    PENDING, // 지원서 제출 후 접수 완료
+    PENDING,  // 아직 임시저장 상태로, 제출 이전임.
+    SUBMITTED, // 지원서 제출 완료
 
-    PASSED, // 합격
+    APPROVED, // 합격
     REJECTED, // 불합격
 
     CANCELED // 지원자가 지원을 취소한 경우
