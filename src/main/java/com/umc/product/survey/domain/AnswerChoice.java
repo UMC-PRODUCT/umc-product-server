db069e2248e6bac9174fe49b97c8aa717c8c4bdb
galllee <kimim0410@gmail.com>
2026-04-13 03:38:43 +0900

    feat: Answer, AnswerChoice 생성 및 QuestionOption content length 추가

---
+++ src/main/java/com/umc/product/survey/domain/AnswerChoice.java
@@ -0,0 +1,39 @@
+package com.umc.product.survey.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name = "answer_choice")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AnswerChoice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "answer_id", nullable = false)
+    private Answer answer;
+
+    // 길이 변경 시 QuestionOption.content도 함께 변경
+    @Column(name = "answered_as_content", nullable = false, length = 500)
+    private String answeredAsContent;
+
+    // 사용자가 선택한 '보기'의 ID를 참조
+    // TODO: repository에 JPQL 만들기
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_option_id")
+    @OnDelete(action = OnDeleteAction.SET_NULL)
+    private QuestionOption questionOption;
+
+    public AnswerChoice(Answer answer, QuestionOption questionOption) {
+        this.answer = answer;
+        this.questionOption = questionOption;
+    }
+}
