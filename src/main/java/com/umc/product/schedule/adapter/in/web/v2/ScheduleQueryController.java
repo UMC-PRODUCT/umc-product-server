c38bcdc0b9ab7bf575ce50e241bed078ef7ebc56
Kyeoung Woon Park <saveearth1@cau.ac.kr>
2026-04-12 10:02:41 +0900

    feat: 일정 조회 시 필터링 가능한 쿼리 파라미터 추가 및 제출한 사유를 조회할 수 있도록 변경

--- src/main/java/com/umc/product/schedule/adapter/in/web/v2/ScheduleQueryController.java
+++ src/main/java/com/umc/product/schedule/adapter/in/web/v2/ScheduleQueryController.java
@@ -4,6 +4,7 @@ import com.umc.product.global.exception.NotImplementedException;
 import com.umc.product.global.security.MemberPrincipal;
 import com.umc.product.global.security.annotation.CurrentMember;
 import com.umc.product.schedule.adapter.in.web.v2.dto.response.ScheduleInfoResponse;
+import com.umc.product.schedule.domain.enums.AttendanceStatus;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.time.Instant;
@@ -26,6 +27,8 @@ public class ScheduleQueryController {
     @Operation(summary = "내 일정 조회", description = """
         로그인한 사용자가 참여하는 일정 중 Query Param의 `from`, `to` 사이에 시작일이 있는 일정을 모두 조회합니다.
 
+        활동-출석 체크 UI에서 활용하기 위해서는 `isAttendanceRequired` 필드를 `true`로 해서 출석을 트래킹하는 API에 대해서만 조회하면 됩니다.
+
         운영진이 출석 승인 대기가 필요한 일정을 조회하는 부분은 다른 API를 이용해주세요.
         """
     )
@@ -33,6 +36,7 @@ public class ScheduleQueryController {
     public List<ScheduleInfoResponse> mySchedule(
         @RequestParam Instant from,
         @RequestParam Instant to,
+        @RequestParam Boolean isAttendanceRequired,
         @CurrentMember MemberPrincipal memberPrincipal
     ) {
         throw new NotImplementedException();
@@ -52,31 +56,26 @@ public class ScheduleQueryController {
 
     // ========================= 출석 관련 =========================
 
-    // 출석 가능한 세션을 조회하는 API
-    @Operation(summary = "일정 상세 조회", description = """
-        단일 일정에 대한 정보를 상세하게 조회합니다.
-        일정의 기본 정보 및 참여자에 대한 정보를 포함해서 전송합니다.
-
-        참여자의 출석 현황 등에 대해서는 별도의 출석 현황 확인 API를 이용해 주세요.
-        """
-    )
-    @GetMapping("/me/available")
-    public ScheduleInfoResponse availableSchedule(@CurrentMember MemberPrincipal memberPrincipal) {
-        throw new NotImplementedException();
-    }
-
     // 운영진용: 각 일정에 대한 출석 현황을 조회하는 API, from-to로 기간을 조회할 수 있어야 합니다.
     // 조회 기간과 무관하게 과거 일정 중에서 출석을 승인하지 않은 일정은 계속 표시됩니다.
     // 사유는 제공된 경우에만 표시됩니다.
     @Operation(summary = "[운영진용] 단일 일정 출석 현황 조회", description = """
-        Query Param의 `from` ~ `to` 사이에 시작 시간이 있는 일정에 대해서 출석 현황을 제공합니다.
-        출석 요청 및 승인 여부에 관계 없이 모두 표시됩니다.
+        Query Param을 이용해서 상세한 필터링을 제공하며, 그 기준은 아래와 같습니다.
+
+        #### 기간 필터링 (시작 시간 기준)
+        `from` ~ `to` 사이에 시작 시간이 있는 일정에 대해서 출석 현황을 제공합니다.
+        기간을 별도로 지정하지 않은 경우, 기본값은 "요청 시점으로부터 1개월 전" ~ "요청 시점으로부터 24시간 후" 입니다.
+
+        #### 출석 상태 필터링
+        `attendanceStatus`를 통해서 요청 상태를 필터링할 수 있습니다.
+        제공되지 않은 경우, 모든 상태에 대해서 반환합니다.
         """
     )
     @GetMapping("/{scheduleId}/attendance")
     public ScheduleInfoResponse getAttendanceInfo(
         @RequestParam Instant from,
         @RequestParam Instant to,
+        @RequestParam AttendanceStatus attendanceStatus,
         @CurrentMember MemberPrincipal memberPrincipal,
         @PathVariable String scheduleId
     ) {
