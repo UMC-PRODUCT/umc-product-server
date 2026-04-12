2debec691f214407a5323fc5fb249ccaaffde1bc
galllee <kimim0410@gmail.com>
2026-04-13 01:11:41 +0900

    remove: recruitment 도메인에서 사용하던 usecase 제거

--- src/main/java/com/umc/product/survey/application/port/in/command/dto/SubmitFormResponseCommand.java
+++
@@ -1,7 +0,0 @@
-package com.umc.product.survey.application.port.in.command.dto;
-
-public record SubmitFormResponseCommand(
-        Long memberId,
-        Long formResponseId
-) {
-}
--- src/main/java/com/umc/product/survey/application/port/in/query/dto/GetFormDefinitionQuery.java
+++
@@ -1,6 +0,0 @@
-package com.umc.product.survey.application.port.in.query.dto;
-
-public record GetFormDefinitionQuery(
-        Long formId
-) {
-}
