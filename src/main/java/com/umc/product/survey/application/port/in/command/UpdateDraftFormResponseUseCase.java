04cd65509c5c731789d3430699fa01b214e8a126
galllee <kimim0410@gmail.com>
2026-04-13 01:00:28 +0900

    remove: 구현체가 없는 usecase 제거

--- src/main/java/com/umc/product/survey/application/port/in/command/UpdateDraftFormResponseUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.umc.product.survey.application.port.in.command;
-
-import com.umc.product.survey.application.port.in.command.dto.UpdateDraftFormResponseCommand;
-
-public interface UpdateDraftFormResponseUseCase {
-    void update(UpdateDraftFormResponseCommand command);
-}
