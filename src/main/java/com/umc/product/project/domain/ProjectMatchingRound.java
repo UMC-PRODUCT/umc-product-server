b0e4d4ff13df0c835a3cc6be9fe57e50ac1399a2
Kyeoung Woon Park <saveearth1@cau.ac.kr>
2026-04-12 23:53:42 +0900

    feat: 매칭 차수에 UK 제약 추가 및 주석 추가

--- src/main/java/com/umc/product/project/domain/ProjectMatchingRound.java
+++ src/main/java/com/umc/product/project/domain/ProjectMatchingRound.java
@@ -10,6 +10,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import java.time.Instant;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -24,7 +25,16 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "project_matching_round")
+@Table(
+    name = "project_matching_round",
+    uniqueConstraints = {
+        @UniqueConstraint(
+            name = "uk_project_matching_round_type_phase_chapter",
+            // 동일한 매칭 종류와 차수를 가진건 지부에 하나만 있어야 해요!
+            columnNames = {"type", "phase", "chapterId"}
+        )
+    }
+)
 public class ProjectMatchingRound extends BaseEntity {
 
     @Id
@@ -36,13 +46,17 @@ public class ProjectMatchingRound extends BaseEntity {
     private String description; // 매칭 차수 설명
 
     @Enumerated(EnumType.STRING)
-    private MatchingType type; // 매칭 차수의 종류
+    private MatchingType type; // 매칭의 종류
 
-    // 몇 차 매칭인지
-    // 고민사항 : 처음에는 Long 값으로 phaseNo로 넣었었는데, 매칭 관련해서는 확장성이 사실 상 필요 없는 점을 고려하였음.
-    // 또한 랜덤매칭의 경우, UI 상에 표시해줄 필요성은 있다고 생각하는데 .. DB에서 THIRD가 지난 상태를 랜덤매칭으로 지정할지가 고민 ..
+    /*
+    몇 차 매칭인지
+    고민사항 : 처음에는 Long 값으로 phaseNo로 넣었었는데, 매칭 관련해서는 확장성이 사실 상 필요 없는 점을 고려하였음.
+    또한 랜덤매칭의 경우, UI 상에 표시해줄 필요성은 있다고 생각하는데 .. DB에서 THIRD가 지난 상태를 랜덤매칭으로 지정할지가 고민 ..
+
+    -> 랜덤매칭에 최초에 기간이 필요했던 이유는 사람이 일을 해야하니까인데.. 지금은 그냥 랜덤매칭 딸깎 하면 되는거라 enum에 넣을 필요는 없을 듯
+     */
     @Enumerated(EnumType.STRING)
-    private MatchingPhase phase;
+    private MatchingPhase phase; // 매칭 차수
 
     private Long chapterId;
 
@@ -55,7 +69,7 @@ public class ProjectMatchingRound extends BaseEntity {
     private Instant autoDecisionExecutedAt;
     // 자동 선발이 실행된 경우, 누가 실행했는지 (운영진 ID)
     // 스케쥴러가 진행한 경우 null로 유지함
-    private Long autoDecisionExecutedBy;
+    private Long autoDecisionExecutedMemberId;
 
     @Builder(access = AccessLevel.PRIVATE)
     private ProjectMatchingRound(
