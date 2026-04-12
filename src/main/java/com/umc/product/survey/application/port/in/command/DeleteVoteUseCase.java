a414fee42a5b87f1416455459a0d93443260c789
galllee <kimim0410@gmail.com>
2026-04-13 01:34:55 +0900

    remove: survey port.in 삭제

--- src/main/java/com/umc/product/survey/application/port/in/command/DeleteVoteUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.umc.product.survey.application.port.in.command;
-
-import com.umc.product.survey.application.port.in.command.dto.DeleteVoteCommand;
-
-public interface DeleteVoteUseCase {
-    void delete(DeleteVoteCommand command);
-}
--- src/main/java/com/umc/product/survey/application/port/in/command/dto/DeleteVoteCommand.java
+++
@@ -1,7 +0,0 @@
-package com.umc.product.survey.application.port.in.command.dto;
-
-public record DeleteVoteCommand(
-    Long voteId,
-    Long memberId
-) {
-}
