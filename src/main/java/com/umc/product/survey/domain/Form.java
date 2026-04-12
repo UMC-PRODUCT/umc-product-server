5d91f9be76e5ce9d80241dcb065a4e89c37e1e93
galllee <kimim0410@gmail.com>
2026-04-13 01:40:28 +0900

    remove: form -> formSection OneToMany 종속 제거 및 Vote 관련 도메인 메소드 제거

--- src/main/java/com/umc/product/survey/domain/Form.java
+++ src/main/java/com/umc/product/survey/domain/Form.java
@@ -1,19 +1,11 @@
 package com.umc.product.survey.domain;
 
 import com.umc.product.common.BaseEntity;
-import com.umc.product.survey.domain.enums.FormOpenStatus;
-import com.umc.product.survey.domain.enums.FormSectionType;
 import com.umc.product.survey.domain.enums.FormStatus;
-import com.umc.product.survey.domain.enums.QuestionType;
 import com.umc.product.survey.domain.exception.SurveyDomainException;
 import com.umc.product.survey.domain.exception.SurveyErrorCode;
 import jakarta.persistence.*;
 import lombok.*;
-
-import java.time.Instant;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
 
 @Entity
 @Builder
@@ -43,15 +35,11 @@ public class Form extends BaseEntity {
     @Column(name = "is_anonymous", nullable = false)
     private boolean isAnonymous;
 
-    @OneToMany(mappedBy = "form", cascade = CascadeType.ALL, orphanRemoval = true)
-    @OrderBy("orderNo ASC")
-    @Builder.Default
-    private Set<FormSection> sections = new LinkedHashSet<>();
-
     public static Form createDraft(Long createdMemberId) {
         Form form = new Form();
         form.createdMemberId = createdMemberId;
         form.status = FormStatus.DRAFT;
+
         return form;
     }
 
@@ -60,6 +48,7 @@ public class Form extends BaseEntity {
         form.createdMemberId = createdMemberId;
         form.title = title;
         form.status = FormStatus.PUBLISHED;
+
         return form;
     }
 
@@ -71,42 +60,7 @@ public class Form extends BaseEntity {
         if (isPublished()) {
             throw new SurveyDomainException(SurveyErrorCode.SURVEY_ALREADY_PUBLISHED);
         }
+
         this.status = FormStatus.PUBLISHED;
-    }
-
-    @Deprecated
-    // TODO: NoticeVote 쪽 도메인 메소드로
-    public void setVotePolicy(boolean isAnonymous, Instant startsAt, Instant endsAtExclusive) {
-        return;
-    }
-
-    // TODO: NoticeVote 쪽 도메인 메소드로
-    public FormOpenStatus getOpenStatus(Instant now) {
-        return null;
-    }
-
-    /**
-     * 투표는 섹션 1개 / 질문 1개를 생성하고, options 순서대로 orderNo=1..N 부여.
-     */
-    public void appendSingleQuestion(String questionText, QuestionType type, List<String> optionContents) {
-        int sectionOrderNo = this.sections.size() + 1;
-
-        FormSection section = FormSection.create(
-            this,
-            FormSectionType.DEFAULT,
-            null,
-            this.title,
-            sectionOrderNo
-        );
-        this.sections.add(section);
-
-        int questionOrderNo = 1;
-        Question q = Question.create(questionText, type, true, questionOrderNo);
-        section.addQuestion(q);
-
-        int optionOrderNo = 1;
-        for (String content : optionContents) {
-            q.addOption(QuestionOption.create(content, optionOrderNo++, false));
-        }
     }
 }
