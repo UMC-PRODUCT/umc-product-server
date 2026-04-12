5f2fcdcf91814a0114aa17b83e3fb456999af22a
Kyeoung Woon Park <saveearth1@cau.ac.kr>
2026-04-12 09:34:46 +0900

    feat: 출석 현황 조회 API 관련 DTO 제작

--- src/main/java/com/umc/product/schedule/adapter/in/web/v2/ScheduleQueryController.java
+++ src/main/java/com/umc/product/schedule/adapter/in/web/v2/ScheduleQueryController.java
@@ -1,18 +1,27 @@
 package com.umc.product.schedule.adapter.in.web.v2;
 
+import com.umc.product.global.exception.NotImplementedException;
+import com.umc.product.global.security.MemberPrincipal;
+import com.umc.product.global.security.annotation.CurrentMember;
 import com.umc.product.schedule.adapter.in.web.v2.dto.response.ScheduleInfoResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.time.Instant;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v2/schedules")
 @RequiredArgsConstructor
+@Tag(name = "Schedule V2 | Query", description = "일정 및 출석 관련 내용들을 조회합니다.")
 public class ScheduleQueryController {
+
+    // ========================= 일정 관련 =========================
 
     @Operation(summary = "내 일정 조회", description = """
         로그인한 사용자가 참여하는 일정 중 Query Param의 `from`, `to` 사이에 시작일이 있는 일정을 모두 조회합니다.
@@ -21,8 +30,12 @@ public class ScheduleQueryController {
         """
     )
     @GetMapping("/me")
-    public List<ScheduleInfoResponse> mySchedule() {
-        return null;
+    public List<ScheduleInfoResponse> mySchedule(
+        @RequestParam Instant from,
+        @RequestParam Instant to,
+        @CurrentMember MemberPrincipal memberPrincipal
+    ) {
+        throw new NotImplementedException();
     }
 
     @Operation(summary = "일정 상세 조회", description = """
@@ -34,7 +47,40 @@ public class ScheduleQueryController {
     )
     @GetMapping("/{scheduleId}")
     public ScheduleInfoResponse details(@PathVariable String scheduleId) {
-        return null;
+        throw new NotImplementedException();
+    }
+
+    // ========================= 출석 관련 =========================
+
+    // 출석 가능한 세션을 조회하는 API
+    @Operation(summary = "일정 상세 조회", description = """
+        단일 일정에 대한 정보를 상세하게 조회합니다.
+        일정의 기본 정보 및 참여자에 대한 정보를 포함해서 전송합니다.
+
+        참여자의 출석 현황 등에 대해서는 별도의 출석 현황 확인 API를 이용해 주세요.
+        """
+    )
+    @GetMapping("/me/available")
+    public ScheduleInfoResponse availableSchedule(@CurrentMember MemberPrincipal memberPrincipal) {
+        throw new NotImplementedException();
+    }
+
+    // 운영진용: 각 일정에 대한 출석 현황을 조회하는 API, from-to로 기간을 조회할 수 있어야 합니다.
+    // 조회 기간과 무관하게 과거 일정 중에서 출석을 승인하지 않은 일정은 계속 표시됩니다.
+    // 사유는 제공된 경우에만 표시됩니다.
+    @Operation(summary = "[운영진용] 단일 일정 출석 현황 조회", description = """
+        Query Param의 `from` ~ `to` 사이에 시작 시간이 있는 일정에 대해서 출석 현황을 제공합니다.
+        출석 요청 및 승인 여부에 관계 없이 모두 표시됩니다.
+        """
+    )
+    @GetMapping("/{scheduleId}/attendance")
+    public ScheduleInfoResponse getAttendanceInfo(
+        @RequestParam Instant from,
+        @RequestParam Instant to,
+        @CurrentMember MemberPrincipal memberPrincipal,
+        @PathVariable String scheduleId
+    ) {
+        throw new NotImplementedException();
     }
 
 }
--- src/main/java/com/umc/product/schedule/adapter/in/web/v2/dto/response/ScheduleInfoResponse.java
+++ src/main/java/com/umc/product/schedule/adapter/in/web/v2/dto/response/ScheduleInfoResponse.java
@@ -1,10 +1,14 @@
 package com.umc.product.schedule.adapter.in.web.v2.dto.response;
 
+import com.umc.product.schedule.domain.enums.AttendanceStatus;
 import com.umc.product.schedule.domain.enums.ScheduleTag;
 import java.time.Instant;
 import java.util.List;
 import java.util.Set;
+import lombok.AccessLevel;
+import lombok.Builder;
 
+@Builder(access = AccessLevel.PRIVATE)
 public record ScheduleInfoResponse(
     Long scheduleId,
     String name,
@@ -17,11 +21,21 @@ public record ScheduleInfoResponse(
     boolean isOnline, // 비대면 일정인지 여부, 즉 location이 null인지 여부를 나타냄
     ScheduleLocationInfoResponse location,
     // 출석 관련
+    // 해당 일정에 대한 요청자의 출석 상태입니다. 요청한 사람이 참여자가 아니거나 아직 출석 요청을 하지 않은 경우 null로 갑니다.
+    AttendanceStatus attendanceStatus,
     boolean isAttendanceChecked, // 출석을 체크하는 일정인지, 즉 attendancePolicy의 null 여부를 나타냄
     ScheduleAttendancePolicyInfoResponse attendancePolicy,
     // 참여자 관련
+    boolean isParticipant, // 요청한 사용자가 해당 일정의 참여자인지 여부입니다. 참여자 목록에 요청한 사용자가 포함되어 있는지 여부와는 별개로, 서버 측에서 별도로 계산해서 제공합니다.
     List<ScheduleParticipantInfoResponse> participants
 ) {
+    public static ScheduleInfoResponse of(
+        // ScheduleInfo와 같이 UseCase단 Info 객체를 이용해서 정팩메를 생성해주세요.
+    ) {
+        // builder 활용하기!
+        return ScheduleInfoResponse.builder().build();
+    }
+
     public record ScheduleLocationInfoResponse(
         Double latitude,
         Double longitude,
@@ -30,11 +44,8 @@ public record ScheduleInfoResponse(
     }
 
     public record ScheduleAttendancePolicyInfoResponse(
-        // 아래 3개의 필드는 AttendancePolicy에서 그대로 가지고 올 것
-        Long earlyCheckInMinutes,
-        Long lateCheckInMinutes,
-        Long lateToleranceMinutes,
-        // Client에서 편하게 할 수 있도록 서버에서 위 3개의 값을 기반으로 연산해서 제공할 것
+        // Client단을 위해서, 서버 측에서 earlyCheckInMinutes, lateCheckInMinutes, lateToleranceMinutes를 기반으로
+        // 아래 3가지 값을 계산해서 제공하도록 합니다.
         Instant checkInStartAt,
         Instant onTimeEndAt,
         Instant lateEndAt
