e1a7b0b4ab853786758b87682d9e06b4b2b6b711
seoungbeom <psb3707@naver.com>
2026-04-24 17:33:00 +0900

    feat: Curriculum 도메인 CreateOriginalWorkbookRequest DTO 추가 (검증조건, 생성메서드 추가)

--- src/main/java/com/umc/product/curriculum/adapter/in/web/v2/dto/request/CreateOriginalWorkbookRequest.java
+++ src/main/java/com/umc/product/curriculum/adapter/in/web/v2/dto/request/CreateOriginalWorkbookRequest.java
@@ -1,6 +1,13 @@
 package com.umc.product.curriculum.adapter.in.web.v2.dto.request;
 
+import com.umc.product.curriculum.application.port.in.command.dto.workbook.CreateOriginalWorkbookCommand;
+import com.umc.product.curriculum.domain.enums.OriginalWorkbookStatus;
+import com.umc.product.curriculum.domain.enums.OriginalWorkbookType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record CreateOriginalWorkbookRequest(
+
     @NotNull(message = "주차별 커리큘럼 ID는 필수입니다")
     Long weeklyCurriculumId,
 
