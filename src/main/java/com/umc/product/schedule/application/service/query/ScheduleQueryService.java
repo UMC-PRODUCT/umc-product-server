a58262d8aa1516fcbb56519c75faab00c514a062
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2026-04-27 00:47:26 +0900

    feat: 일정이 출석 정책을 가졌는지 여부를 리턴하는 usecase 구현

--- src/main/java/com/umc/product/schedule/application/service/query/ScheduleQueryService.java
+++ src/main/java/com/umc/product/schedule/application/service/query/ScheduleQueryService.java
@@ -162,7 +162,16 @@ public class ScheduleQueryService implements GetScheduleUseCase {
         return new AdminScheduleInfo(createBaseInfo(schedule), adminParticipants);
     }
 
-    // ======= Helper Method =======
+    // 일정이 출석 정책을 가졌는지 여부를 리턴
+    @Override
+    public boolean getHaveAttendancePolicy(Long scheduleId) {
+        Schedule schedule = loadSchedulePort.findById(scheduleId)
+            .orElseThrow(() -> new ScheduleDomainException(ScheduleErrorCode.SCHEDULE_NOT_FOUND));
+
+        return schedule.getPolicy() != null;
+    }
+
+    // =============================== Helper Method ===============================
 
     // ScheduleBaseInfo 변환 메서드
     private ScheduleBaseInfo createBaseInfo(Schedule schedule) {
