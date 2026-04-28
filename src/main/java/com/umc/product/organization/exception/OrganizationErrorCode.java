0877ab1493c05098187b162a55b2d4e4ca1e102c
Kyeoung Woon Park <saveearth1@cau.ac.kr>
2026-04-29 06:05:58 +0900

    docs: 어색한 에러코드명 수정

--- src/main/java/com/umc/product/organization/exception/OrganizationErrorCode.java
+++ src/main/java/com/umc/product/organization/exception/OrganizationErrorCode.java
@@ -38,22 +38,25 @@ public enum OrganizationErrorCode implements BaseCode {
     PART_REQUIRED(HttpStatus.BAD_REQUEST, "ORGANIZAITON-0021", "파트는 필수입니다."),
     STUDY_GROUP_NAME_INVALID(HttpStatus.BAD_REQUEST, "ORGANIZAITON-0022", "유효하지 않은 스터디 그룹 이름입니다."),
     STUDY_GROUP_NOT_FOUND(HttpStatus.BAD_REQUEST, "ORGANIZAITON-0023", "스터디 그룹을 찾을 수 없습니다."),
+
     STUDY_GROUP_CHALLENGER_INVALID(HttpStatus.BAD_REQUEST, "ORGANIZATION-0024",
         "스터디 그룹의 리더 또는 멤버로 존재하지 않는 챌린저가 포함되어 있습니다."),
     LEADER_CANNOT_BE_MEMBER(HttpStatus.BAD_REQUEST, "ORGANIZATION-0025", "스터디 그룹의 리더는 멤버가 될 수 없습니다."),
     STUDY_GROUP_MEMBER_DUPLICATED(HttpStatus.BAD_REQUEST, "ORGANIZATION-0026", "스터디 그룹 멤버 ID에 중복이 있습니다."),
-    NO_SUCH_CHAPTER_SCHOOL(HttpStatus.NOT_FOUND, "ORGANIZATION-0027", "요청하신 학교와 지부와 일치하는 정보가 없습니다.."),
+    NO_SUCH_CHAPTER_SCHOOL(HttpStatus.NOT_FOUND, "ORGANIZATION-0027", "요청하신 학교와 지부와 일치하는 정보가 없습니다."),
     GISU_ALREADY_EXISTS(HttpStatus.CONFLICT, "ORGANIZATION-0028", "이미 존재하는 기수입니다."),
     SCHOOL_ALREADY_ASSIGNED_TO_CHAPTER(HttpStatus.CONFLICT, "ORGANIZATION-0029",
         "해당 기수에서 이미 다른 지부에 배정된 학교가 포함되어 있습니다."),
     CHAPTER_NAME_DUPLICATED(HttpStatus.CONFLICT, "ORGANIZATION-0030", "해당 기수에 동일한 이름의 지부가 이미 존재합니다."),
+
     STUDY_GROUP_ACCESS_DENIED(HttpStatus.FORBIDDEN, "ORGANIZATION-0031", "스터디 그룹 조회 권한이 없습니다."),
     GISU_HAS_ASSOCIATED_CHAPTERS(HttpStatus.CONFLICT, "ORGANIZATION-0032", "해당 기수에 연결된 지부 또는 학교가 존재하여 삭제할 수 없습니다."),
-    STUDY_GROUP_MENTOR_REQUIRED(HttpStatus.BAD_REQUEST, "ORGANIZATION-0033", "스터디 그룹 파트장는 최소 1명 이상이어야 합니다."),
-    STUDY_GROUP_MENTOR_ID_REQUIRED(HttpStatus.BAD_REQUEST, "ORGANIZATION-0034" , "스터디 그룹 멘토의 StudyGroupMentor ID는 필수입니다."),
+    STUDY_GROUP_MENTOR_REQUIRED(HttpStatus.BAD_REQUEST, "ORGANIZATION-0033", "스터디 그룹 파트장은 최소 1명 이상이어야 합니다."),
+    STUDY_GROUP_MENTOR_ID_REQUIRED(HttpStatus.BAD_REQUEST, "ORGANIZATION-0034",
+        "스터디 그룹 멘토의 ID는 필수입니다."),
     STUDY_GROUP_MEMBER_ALREADY_IN_PART_STUDY(HttpStatus.CONFLICT, "ORGANIZATION-0035", "다른 스터디 그룹과 중복된 멤버가 있습니다."),
-    STUDY_GROUP_MENTOR_DUPLICATED(HttpStatus.BAD_REQUEST, "ORGANIZATION-0036", "스터디 그룹 멘토 ID에 중복이 있습니다."),
-    STUDY_GROUP_MENTOR_NOT_FOUND(HttpStatus.NOT_FOUND, "ORGANIZATION-0037", "스터디 그룹 멘토를 찾을 수 없습니다.");
+    STUDY_GROUP_MENTOR_DUPLICATED(HttpStatus.BAD_REQUEST, "ORGANIZATION-0036", "이미 해당 스터디에 속한 파트장입니다."),
+    STUDY_GROUP_MENTOR_NOT_FOUND(HttpStatus.NOT_FOUND, "ORGANIZATION-0037", "스터디 그룹의 파트장 정보를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
