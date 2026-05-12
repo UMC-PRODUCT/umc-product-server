e1323cb23726ab8d03061e383a20879062d5be14
Kwon-DoHee <152317074+seamooll@users.noreply.github.com>
2026-05-12 21:30:37 +0900

    feat: Inquiry 컨트롤러 스켈레톤 생성

---
+++ src/main/java/com/umc/product/inquiry/adapter/in/web/InquiryWebSocketController.java
@@ -0,0 +1,12 @@
+package com.umc.product.inquiry.adapter.in.web;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+// STOMP 메시지 수신 처리 컨트롤러. 실제 메시지 전송 엔드포인트는 API 구현 이슈에서 추가 예정
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class InquiryWebSocketController {
+}
