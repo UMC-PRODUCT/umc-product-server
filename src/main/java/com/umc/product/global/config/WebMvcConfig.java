d8e888caa031e92d7a312f565236fe94bd2cccd8
Kyeoung Woon Park <saveearth1@cau.ac.kr>
2026-05-25 02:44:48 +0900

    fix: redirect documentation catalog paths

--- src/main/java/com/umc/product/global/config/WebMvcConfig.java
+++ src/main/java/com/umc/product/global/config/WebMvcConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -18,6 +19,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(currentMemberArgumentResolver);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/docs/catalog/api", "/docs/catalog/api/");
+        registry.addRedirectViewController("/docs/catalog/error", "/docs/catalog/error/");
     }
 
     @Override
