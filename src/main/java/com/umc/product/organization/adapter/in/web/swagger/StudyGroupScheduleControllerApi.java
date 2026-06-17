8333c250095d5f6be99e82be2e0f00024f64cc4f
Kyeoung Woon Park <saveearth1@cau.ac.kr>
2026-06-18 05:16:49 +0900

    docs: StudyGroupScheduleControllerApi OpenAPI 문구 정리

--- src/main/java/com/umc/product/organization/adapter/in/web/swagger/StudyGroupScheduleControllerApi.java
+++ src/main/java/com/umc/product/organization/adapter/in/web/swagger/StudyGroupScheduleControllerApi.java
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Organization | 스터디 그룹 일정 Command", description = "스터디 그룹 일정을 생성합니다.")
 public interface StudyGroupScheduleControllerApi {
-    @Operation(summary = "[STUDY-GROUP-SCHEDULE-001] 스터디 그룹 일정 생성", description = """
+    @Operation(operationId = "STUDY-GROUP-SCHEDULE-001", summary = "스터디 그룹 일정 생성", description = """
         스터디 그룹 일정을 생성합니다.
         **'Schedule V2 | Command'**의 **'일정 생성 API'**를 사용하신 후 이 API를 호출해주세요.
 
