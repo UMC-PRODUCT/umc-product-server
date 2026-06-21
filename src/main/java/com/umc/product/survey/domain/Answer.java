83fae8f5dcea43de81dda6ff6caac5c12c5c61ed
galllee <kimim0410@gmail.com>
2026-06-21 13:22:15 +0900

    chore: spotless 임포트 수정

--- src/main/java/com/umc/product/survey/domain/Answer.java
+++ src/main/java/com/umc/product/survey/domain/Answer.java
@@ -1,16 +1,28 @@
 package com.umc.product.survey.domain;
 
-import com.umc.product.common.BaseEntity;
-import com.umc.product.survey.domain.enums.QuestionType;
-import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import java.time.Instant;
+import java.util.Set;
+
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
-import java.time.Instant;
-import java.util.Set;
+import com.umc.product.common.BaseEntity;
+import com.umc.product.survey.domain.enums.QuestionType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 한 사용자가 한 질문에 대해 작성한 답변의 루트 엔티티.
