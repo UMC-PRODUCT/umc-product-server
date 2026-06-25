7840e949482223b7b49a306b54a47a60331e3351
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2026-06-25 14:49:18 +0900

    feat: 프로젝트 멤버 hard delete와 상태 변경(soft delete) API 분리

--- src/main/java/com/umc/product/project/application/port/in/command/RemoveProjectMemberUseCase.java
+++ src/main/java/com/umc/product/project/application/port/in/command/RemoveProjectMemberUseCase.java
@@ -3,7 +3,10 @@ package com.umc.product.project.application.port.in.command;
 import com.umc.product.project.application.port.in.command.dto.RemoveProjectMemberCommand;
 
 /**
- * 프로젝트 멤버 제거 UseCase (PROJECT-005).
+ * 프로젝트 멤버 hard delete UseCase (PROJECT-005).
+ * <p>
+ * 멤버 행을 물리적으로 삭제해 동일 멤버의 재등록을 가능하게 한다. 상태 이력 보존이 필요하면
+ * {@link ChangeProjectMemberStatusUseCase}(PROJECT-006) 를 사용한다.
  */
 public interface RemoveProjectMemberUseCase {
 
