58ecfcab829cff3d0b2e3a8d9a94a1b112b193ee
Kyeoung Woon Park <saveearth1@cau.ac.kr>
2026-06-26 15:48:49 +0900

    fix: register rate limit properties in config

--- src/main/java/com/umc/product/global/ratelimit/ApiRateLimitConfig.java
+++ src/main/java/com/umc/product/global/ratelimit/ApiRateLimitConfig.java
@@ -1,11 +1,13 @@
 package com.umc.product.global.ratelimit;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.umc.product.global.response.ApiErrorResponseWriter;
 
 @Configuration
+@EnableConfigurationProperties(ApiRateLimitProperties.class)
 public class ApiRateLimitConfig {
 
     @Bean
